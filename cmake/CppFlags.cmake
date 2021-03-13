e7cc54cf2c8c3b4f78867c28ab38e1036f3b8731
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-03-13 23:42:20 +0900

    chore: add target for this library

---
+++ cmake/CppFlags.cmake
@@ -0,0 +1,49 @@
+include(CheckCXXSourceRuns)
+set(CMAKE_REQUIRED_FLAGS)
+
+if(MSVC)
+    set(CMAKE_REQUIRED_FLAGS "/arch:AVX2")
+else()
+    set(CMAKE_REQUIRED_FLAGS "-mavx2")
+endif()
+
+check_cxx_source_runs(
+    "
+    #include <immintrin.h>
+    int main()
+    {
+        __m256i a, b, c;
+        const int src[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int dst[8];
+        a =  _mm256_loadu_si256( (__m256i*)src );
+        b =  _mm256_loadu_si256( (__m256i*)src );
+        c = _mm256_add_epi32( a, b );
+        _mm256_storeu_si256( (__m256i*)dst, c );
+        for( int i = 0; i < 8; i++ ){
+        if( ( src[i] + src[i] ) != dst[i] ){
+            return -1;
+        }
+        }
+        return 0;
+    }"
+    HAVE_AVX2_EXTENSIONS)
+
+add_library(num_collect_cpp_flags INTERFACE)
+
+if(MSVC)
+    target_compile_options(num_collect_cpp_flags INTERFACE /fp:precise /bigobj)
+else()
+    target_compile_options(num_collect_cpp_flags
+                           INTERFACE -fno-unsafe-math-optimizations)
+endif()
+
+if(HAVE_AVX2_EXTENSIONS)
+    option(NUM_COLLECT_USE_AVX2 "use Intel AVX2 instructions" OFF)
+    if(NUM_COLLECT_USE_AVX2)
+        if(MSVC)
+            target_compile_options(num_collect_cpp_flags INTERFACE /arch:AVX2)
+        else()
+            target_compile_options(num_collect_cpp_flags INTERFACE -mavx2)
+        endif()
+    endif()
+endif()
