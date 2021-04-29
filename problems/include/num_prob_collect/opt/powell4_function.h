a9984c09c91dab00fd2f7214914e021f42b9c8dc
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-04-29 22:53:13 +0900

    test: benchmark with Powell function

---
+++ problems/include/num_prob_collect/opt/powell4_function.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of powell4_function class.
+ */
+#pragma once
+
+#include <Eigen/Core>
+
+namespace num_prob_collect::opt {
+
+/*!
+ * \brief Class of Powell function in 4 dimensions.
+ *
+ * This function has no local minimum except for the global one at
+ * (0, 0, 0, 0).
+ * Search region is [-4, 5] for each dimension.
+ *
+ * Reference:
+ * http://www-optima.amp.i.kyoto-u.ac.jp/member/student/hedar/Hedar_files/TestGO_files/Page2720.htm
+ */
+class powell4_function {
+public:
+    //! Type of variables.
+    using variable_type = Eigen::Vector4d;
+
+    //! Type of function values.
+    using value_type = double;
+
+    /*!
+     * \brief Evaluate function value on variable.
+     *
+     * \param[in] x Variable.
+     */
+    void evaluate_on(const Eigen::VectorXd& x) {
+        value_ = std::pow(x(0) + 10.0 * x(1), 2)  // NOLINT
+            + 5.0 * std::pow(x(2) - x(3), 2)      // NOLINT
+            + std::pow(x(1) - 2.0 * x(2), 4)      // NOLINT
+            + 10.0 * std::pow(x(0) - x(3), 4);    // NOLINT
+
+        grad_(0) = 2.0 * (x(0) + 10.0 * x(1))        // NOLINT
+            + 40.0 * std::pow(x(0) - x(3), 3);       // NOLINT
+        grad_(1) = 20.0 * (x(0) + 10.0 * x(1))       // NOLINT
+            + 4.0 * std::pow(x(1) - 2.0 * x(2), 3);  // NOLINT
+        grad_(2) = 10.0 * (x(2) - x(3))              // NOLINT
+            - 8.0 * std::pow(x(1) - 2.0 * x(2), 3);  // NOLINT
+        grad_(3) = -10.0 * (x(2) - x(3))             // NOLINT
+            - 40.0 * std::pow(x(0) - x(3), 3);       // NOLINT
+    }
+
+    /*!
+     * \brief Get function value.
+     *
+     * \return Function value.
+     */
+    [[nodiscard]] auto value() const -> double { return value_; }
+
+    /*!
+     * \brief Get gradient.
+     *
+     * \return Gradient.
+     */
+    [[nodiscard]] auto gradient() const -> const Eigen::Vector4d& {
+        return grad_;
+    }
+
+private:
+    //! Function value.
+    double value_{};
+
+    //! Gradient.
+    Eigen::Vector4d grad_{};
+};
+
+}  // namespace num_prob_collect::opt
