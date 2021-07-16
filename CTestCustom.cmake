988cc8fb5fd2f767752b3bda0ea94fb0e0e3ce53
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-07-17 00:21:32 +0900

    chore: exclude constexpr functions from coverage measurements

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,1 +1,2 @@
-set(CTEST_CUSTOM_COVERAGE_EXCLUDE "(extern|test)/")
+set(CTEST_CUSTOM_COVERAGE_EXCLUDE
+    "(extern|test|include/num_collect/constants)/")
--- scripts/collect_gcc_coverage.sh
+++ scripts/collect_gcc_coverage.sh
@@ -11,5 +11,6 @@ ROOT=$(realpath $(dirname $(dirname $0)))
 
 lcov --rc lcov_branch_coverage=1 --directory ./ --capture --output-file $COV
 lcov --rc lcov_branch_coverage=1 --extract $COV "${ROOT}/include/*" --output-file $COV
+lcov --rc lcov_branch_coverage=1 --remove $COV "${ROOT}/include/num_collect/constants/" --output-file $COV
 genhtml --rc lcov_branch_coverage=1 --output-directory $HTML $COV
 lcov --rc lcov_branch_coverage=1 --list $COV
