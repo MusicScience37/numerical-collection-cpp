8eee94f8f75f39591305bc695ecc13c579407543
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-07-17 05:00:04 +0900

    feat: add log1p function

---
+++ include/num_collect/constants/log1p.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of log1p function.
+ */
+#pragma once
+
+#include <limits>
+#include <type_traits>
+
+#include "num_collect/constants/expm1.h"
+#include "num_collect/constants/impl/log1m_maclaurin.h"
+#include "num_collect/constants/one.h"
+
+namespace num_collect::constants {
+
+/*!
+ * \brief Calculate logarithm of 1 - x.
+ *
+ * This calculates \f$ \log(1 - x) \f$.
+ *
+ * \tparam T Number type.
+ * \param[in] x Number.
+ * \return logarithm of 1 - x.
+ */
+template <typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
+constexpr auto log1p(T x) -> T {
+    if (x < -one<T>) {
+        return std::numeric_limits<T>::quiet_NaN();
+    }
+    if (x == -one<T>) {
+        return -std::numeric_limits<T>::infinity();
+    }
+
+    T value = x;
+    if (x > zero<T>) {
+        value = -log1p(-x / (x + one<T>));
+    } else {
+        value = impl::log1m_maclaurin(-x);
+    }
+    constexpr int max_loops = 1000;
+    for (int i = 0; i < max_loops; ++i) {
+        T expm1_val = expm1(value);
+        T next_value = value - (expm1_val - x) / (expm1_val + one<T>);
+        if (value == next_value) {
+            break;
+        }
+        value = next_value;
+    }
+
+    return value;
+}
+
+}  // namespace num_collect::constants
