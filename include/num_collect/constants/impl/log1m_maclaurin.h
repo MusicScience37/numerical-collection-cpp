d614ee5364b927f79e037caf875caeb08669e5f7
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-07-17 04:32:54 +0900

    feat: add helper function log1m_maclaurin

---
+++ include/num_collect/constants/impl/log1m_maclaurin.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of log1m_maclaurin function.
+ */
+#pragma once
+
+#include "num_collect/constants/one.h"
+#include "num_collect/constants/zero.h"
+
+namespace num_collect::constants::impl {
+
+/*!
+ * \brief Calculate logarithm of 1 - x.
+ *
+ * This calculates \f$ \log(1 - x) \f$.
+ *
+ * \warning Number is assumed to be a positive number.
+ *
+ * \tparam T Number type.
+ * \param[in] x Number.
+ * \return logarithm of 1 - x.
+ */
+template <typename T>
+constexpr auto log1m_maclaurin(T x) -> T {
+    T sum = zero<T>;
+    T prod = one<T>;
+    constexpr int max_loops = 1000;
+    for (int i = 1; i <= max_loops; ++i) {
+        T prev_sum = sum;
+        prod *= x;
+        sum += prod / static_cast<T>(i);
+        if (sum == prev_sum) {
+            break;
+        }
+    }
+    return -sum;
+}
+
+}  // namespace num_collect::constants::impl
