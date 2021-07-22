8bf35cec39abd8087143ee171f6d4f040937f684
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-07-22 22:49:40 +0900

    feat: double exponential rule for infinite range

---
+++ include/num_collect/integration/de_infinite_integrator.h
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of de_infinite_integrator function.
+ */
+#pragma once
+
+#include <cmath>
+#include <type_traits>
+
+#include "num_collect/constants/half.h"
+#include "num_collect/constants/one.h"
+#include "num_collect/constants/pi.h"
+#include "num_collect/constants/zero.h"
+#include "num_collect/util/assert.h"
+#include "num_collect/util/index_type.h"
+
+namespace num_collect::integration {
+
+/*!
+ * \brief Class to perform numerical integration on infinite range \f$(-\infty,
+ * \infty)\f$ using double exponential rule.
+ *
+ * \tparam Variable Type of variables.
+ */
+template <typename Variable>
+class de_infinite_integrator {
+public:
+    //! Type of variables.
+    using variable_type = Variable;
+
+    /*!
+     * \brief Construct.
+     */
+    de_infinite_integrator() = default;
+
+    /*!
+     * \brief Integrate a function.
+     *
+     * \tparam Function Type of function.
+     * \tparam Result Type of result.
+     * \param[in] function Function.
+     * \return Result.
+     */
+    template <typename Function,
+        typename Result =
+            std::decay_t<std::invoke_result_t<Function, variable_type>>>
+    [[nodiscard]] auto integrate(const Function& function) const -> Result {
+        const variable_type interval =
+            max_point_ / static_cast<variable_type>(points_);
+
+        constexpr variable_type diff_coeff_center = half_pi;
+        Result sum =
+            function(constants::zero<variable_type>) * diff_coeff_center;
+
+        for (index_type i = 1; i < points_; ++i) {
+            const variable_type changed_var =
+                interval * static_cast<variable_type>(i);
+            const variable_type half_pi_sinh = half_pi * std::sinh(changed_var);
+            const variable_type var = std::sinh(half_pi_sinh);
+            const variable_type diff_coeff =
+                half_pi * std::cosh(half_pi_sinh) * std::cosh(changed_var);
+            sum += function(var) * diff_coeff;
+            sum += function(-var) * diff_coeff;
+        }
+
+        return sum * interval;
+    }
+
+    /*!
+     * \brief Integrate a function.
+     *
+     * \tparam Function Type of function.
+     * \tparam Result Type of result.
+     * \param[in] function Function.
+     * \return Result.
+     */
+    template <typename Function,
+        typename Result =
+            std::decay_t<std::invoke_result_t<Function, variable_type>>>
+    [[nodiscard]] auto operator()(const Function& function) const -> Result {
+        return integrate(function);
+    }
+
+    /*!
+     * \brief Set maximum point in changed variable.
+     *
+     * \param[in] val Value.
+     * \return This.
+     */
+    auto max_point(variable_type val) -> de_infinite_integrator& {
+        NUM_COLLECT_ASSERT(val > constants::zero<variable_type>);
+        max_point_ = val;
+        return *this;
+    }
+
+    /*!
+     * \brief Set number of points.
+     *
+     * \param[in] val Value.
+     * \return This.
+     */
+    auto points(index_type val) -> de_infinite_integrator& {
+        NUM_COLLECT_ASSERT(val > constants::zero<index_type>);
+        points_ = val;
+        return *this;
+    }
+
+private:
+    //! Half value of pi.
+    static constexpr variable_type half_pi =
+        constants::half<variable_type> * constants::pi<variable_type>;
+
+    //! Default maximum point in changed variable.
+    static constexpr auto default_max_point = static_cast<variable_type>(4);
+
+    //! Maximum point in changed variable.
+    variable_type max_point_{default_max_point};
+
+    //! Default number of points.
+    static constexpr index_type default_points = 20;
+
+    //! Number of points.
+    index_type points_{default_points};
+};
+
+}  // namespace num_collect::integration
