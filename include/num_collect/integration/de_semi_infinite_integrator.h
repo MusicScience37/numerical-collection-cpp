c4cc66baffa45945db75c30e6293b4c5e1b130b1
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-07-22 22:25:16 +0900

    feat: double exponential rule for semi-infinite range

---
+++ include/num_collect/integration/de_semi_infinite_integrator.h
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of de_semi_infinite_integrator function.
+ */
+#pragma once
+
+#include <cmath>
+#include <type_traits>
+
+#include "num_collect/constants/half.h"
+#include "num_collect/constants/one.h"
+#include "num_collect/constants/pi.h"
+#include "num_collect/constants/two.h"
+#include "num_collect/constants/zero.h"
+#include "num_collect/util/assert.h"
+#include "num_collect/util/index_type.h"
+
+namespace num_collect::integration {
+
+namespace impl {
+
+/*!
+ * \brief Helper class of constants for use in de_semi_infinite_integrator
+ * class.
+ *
+ * \tparam Variable Type of variables.
+ */
+template <typename Variable>
+struct de_semi_infinite_integrator_traits;
+
+/*!
+ * \brief Implementation of de_semi_infinite_integrator_traits for float.
+ */
+template <>
+struct de_semi_infinite_integrator_traits<float> {
+public:
+    //! Default maximum point in changed variable.
+    static constexpr float default_max_point = 3.0F;
+};
+
+/*!
+ * \brief Implementation of de_semi_infinite_integrator_traits for float.
+ */
+template <>
+struct de_semi_infinite_integrator_traits<double> {
+public:
+    //! Default maximum point in changed variable.
+    static constexpr double default_max_point = 4.0;
+};
+
+}  // namespace impl
+
+/*!
+ * \brief Class to perform numerical integration on semi-infinite range \f$(0,
+ * \infty)\f$ using double exponential rule.
+ *
+ * \tparam Variable Type of variables.
+ */
+template <typename Variable>
+class de_semi_infinite_integrator {
+public:
+    //! Type of variables.
+    using variable_type = Variable;
+
+    /*!
+     * \brief Construct.
+     */
+    de_semi_infinite_integrator() = default;
+
+    /*!
+     * \brief Integrate a function.
+     *
+     * \tparam Function Type of function.
+     * \tparam Result Type of result.
+     * \param[in] function Function.
+     * \return Result.
+     */
+    template <typename Function,
+        typename Result =
+            std::decay_t<std::invoke_result_t<Function, variable_type>>>
+    [[nodiscard]] auto integrate(const Function& function) const -> Result {
+        const variable_type interval =
+            max_point_ / static_cast<variable_type>(points_);
+
+        constexpr variable_type diff_coeff_center =
+            constants::pi<variable_type>;
+        Result sum =
+            function(constants::one<variable_type>) * diff_coeff_center;
+
+        for (index_type i = 1; i < points_; ++i) {
+            const variable_type changed_var =
+                interval * static_cast<variable_type>(i);
+            const variable_type pi_sinh_value =
+                constants::pi<variable_type> * std::sinh(changed_var);
+
+            const variable_type var_plus = std::exp(pi_sinh_value);
+            const variable_type diff_coeff_plus = diff_coeff(changed_var);
+            sum += function(var_plus) * diff_coeff_plus;
+
+            const variable_type var_minus =
+                constants::one<variable_type> / var_plus;
+            const variable_type diff_coeff_minus = diff_coeff(-changed_var);
+            sum += function(var_minus) * diff_coeff_minus;
+        }
+
+        return sum * interval;
+    }
+
+    /*!
+     * \brief Integrate a function.
+     *
+     * \tparam Function Type of function.
+     * \tparam Result Type of result.
+     * \param[in] function Function.
+     * \return Result.
+     */
+    template <typename Function,
+        typename Result =
+            std::decay_t<std::invoke_result_t<Function, variable_type>>>
+    [[nodiscard]] auto operator()(const Function& function) const -> Result {
+        return integrate(function);
+    }
+
+    /*!
+     * \brief Set maximum point in changed variable.
+     *
+     * \param[in] val Value.
+     * \return This.
+     */
+    auto max_point(variable_type val) -> de_semi_infinite_integrator& {
+        NUM_COLLECT_ASSERT(val > constants::zero<variable_type>);
+        max_point_ = val;
+        return *this;
+    }
+
+    /*!
+     * \brief Set number of points.
+     *
+     * \param[in] val Value.
+     * \return This.
+     */
+    auto points(index_type val) -> de_semi_infinite_integrator& {
+        NUM_COLLECT_ASSERT(val > constants::zero<index_type>);
+        points_ = val;
+        return *this;
+    }
+
+private:
+    /*!
+     * \brief Calculate differential coefficient for change of variable.
+     *
+     * \note This assumes that input is a positive number.
+     *
+     * \param[in] changed_var Changed variable.
+     * \return Differential coefficient.
+     */
+    [[nodiscard]] static auto diff_coeff(variable_type changed_var)
+        -> variable_type {
+        return constants::pi<variable_type> *
+            std::exp(constants::pi<variable_type> * std::sinh(changed_var)) *
+            std::cosh(changed_var);
+    }
+
+    //! Default maximum point in changed variable.
+    static constexpr auto default_max_point =
+        impl::de_semi_infinite_integrator_traits<Variable>::default_max_point;
+
+    //! Maximum point in changed variable.
+    variable_type max_point_{default_max_point};
+
+    //! Default number of points.
+    static constexpr index_type default_points = 20;
+
+    //! Number of points.
+    index_type points_{default_points};
+};
+
+}  // namespace num_collect::integration
---
+++ test/units/integration/de_semi_infinite_integrator_test.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Test of de_semi_infinite_integrator class.
+ */
+#include "num_collect/integration/de_semi_infinite_integrator.h"
+
+#include <cmath>
+#include <complex>
+#include <limits>
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating.hpp>
+
+#include "num_collect/constants/napier.h"
+#include "num_collect/constants/one.h"
+#include "num_collect/constants/pi.h"
+
+// NOLINTNEXTLINE
+TEMPLATE_TEST_CASE("num_collect::integration::de_semi_infinite_integrator", "",
+    float, double) {
+    SECTION("integrate exp(-x)") {
+        const auto integrator =
+            num_collect::integration::de_semi_infinite_integrator<TestType>();
+
+        const auto val = integrator([](TestType x) { return std::exp(-x); });
+
+        const auto true_val = static_cast<TestType>(1);
+        constexpr auto tol = static_cast<TestType>(1e-4);
+        REQUIRE_THAT(val, Catch::Matchers::WithinRel(true_val, tol));
+    }
+
+    SECTION("compute gamma(1/2)") {
+        const auto integrator =
+            num_collect::integration::de_semi_infinite_integrator<TestType>();
+
+        const auto val =
+            integrator([](TestType x) { return std::exp(-x) / std::sqrt(x); });
+
+        const auto true_val = std::sqrt(num_collect::constants::pi<TestType>);
+        constexpr auto tol = static_cast<TestType>(1e-4);
+        REQUIRE_THAT(val, Catch::Matchers::WithinRel(true_val, tol));
+    }
+}
