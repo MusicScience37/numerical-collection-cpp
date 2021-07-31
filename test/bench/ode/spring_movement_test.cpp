95755f35af42a780ffd9208217d2f27824732dd1
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-07-31 17:32:28 +0900

    test: benchmark of 2nd order AVF method

--- test/bench/ode/spring_movement_test.cpp
+++ test/bench/ode/spring_movement_test.cpp
@@ -21,6 +21,7 @@
 
 #include "log_energy_change.h"
 #include "log_error_udm.h"
+#include "num_collect/ode/avf/avf2_formula.h"
 #include "num_collect/ode/non_embedded_formula_wrapper.h"
 #include "num_collect/ode/runge_kutta/rk4_formula.h"
 #include "num_collect/ode/runge_kutta/rkf45_formula.h"
@@ -128,6 +129,14 @@ BENCHMARK_F(
     ode_rk_spring_movement, rk4_auto, spring_movement_fixture, 30, 100) {
     using solver_type = num_collect::ode::non_embedded_auto_solver<
         num_collect::ode::runge_kutta::rk4_formula<problem_type>>;
+    auto solver = solver_type(problem_type());
+    perform(solver);
+}
+
+// NOLINTNEXTLINE: external library
+BENCHMARK_F(
+    ode_rk_spring_movement, avf2_auto, spring_movement_fixture, 30, 100) {
+    using solver_type = num_collect::ode::avf::avf2_auto_solver<problem_type>;
     auto solver = solver_type(problem_type());
     perform(solver);
 }
