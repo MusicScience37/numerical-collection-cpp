1d2803313ece93d3f778ad9a7d6928d8e67c3a06
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-08-22 22:15:47 +0900

    chore: check of encoding of files

---
+++ scripts/check_encoding.py
@@ -0,0 +1,14 @@
+"""Check encoding of files.
+"""
+
+from sys import argv
+
+
+def check_encoding(filepath: str) -> str:
+    with open(filepath, mode='r', encoding='utf8') as file:
+        return file.read()
+
+
+if __name__ == '__main__':
+    for arg in argv:
+        check_encoding(arg)
