3ead01030189ba17b179ecc5f77109d1ab077630
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-11-03 14:09:32 +0900

    feat: add custom_float class

---
+++ include/num_collect/numbers/custom_float.h
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of custom_float class.
+ */
+#pragma once
+
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
+#include "num_collect/util/bidirectional_vector.h"
+#include "num_collect/util/index_type.h"
+
+namespace num_collect::numbers {
+
+/*!
+ * \brief Class of floating numbers in a custom number system with given radix.
+ *
+ * \tparam Radix Radix of the number system.
+ * \tparam Digit Type of digits.
+ * \tparam DigitCalc Type of digits for calculation.
+ */
+template <index_type Radix, typename Digit = std::uint8_t,
+    typename DigitCalc = std::int32_t>
+class custom_float {
+public:
+    static_assert(std::is_signed_v<DigitCalc>);
+
+    static_assert(std::numeric_limits<Digit>::max() > Radix);
+    static_assert(std::numeric_limits<DigitCalc>::max() > Radix * Radix);
+    static_assert(std::numeric_limits<DigitCalc>::min() < -Radix * Radix);
+
+    //! Radix of the number system.
+    static constexpr index_type radix = Radix;
+
+    //! Type of digits.
+    using digit_type = Digit;
+
+    //! Type of digits for calculation.
+    using digit_calc_type = DigitCalc;
+
+    /*!
+     * \brief Construct zero.
+     */
+    custom_float()
+        : digits_(typename bidirectional_vector<digit_type>::container_type{0},
+              0) {}
+
+    /*!
+     * \brief Get the index of the lowest digits
+     *
+     * \return Index of lowest digits.
+     */
+    [[nodiscard]] auto lowest_ind() const noexcept -> index_type {
+        return digits_.min_index();
+    }
+
+    /*!
+     * \brief Get the index of the highest digit.
+     *
+     * \return Index of the highest digit.
+     */
+    [[nodiscard]] auto highest_ind() const noexcept -> index_type {
+        return digits_.max_index();
+    }
+
+    /*!
+     * \brief Access a digit with checks.
+     *
+     * \param[in] index Index.
+     * \return Index.
+     */
+    [[nodiscard]] auto at(index_type index) const -> digit_type {
+        return digits_.at(index);
+    }
+
+    /*!
+     * \brief Access a digit without checks.
+     *
+     * \warning Access to indices out of range causes undefined behaviour.
+     *
+     * \param[in] index Index.
+     * \return Index.
+     */
+    [[nodiscard]] auto operator[](index_type index) const -> digit_type {
+        return digits_[index];
+    }
+
+    /*!
+     * \brief Access a digit preparing it if needed.
+     *
+     * \warning Inserted value won't be checked.
+     *
+     * \param[in] index Index.
+     * \return Index.
+     */
+    [[nodiscard]] auto operator[](index_type index) -> digit_type& {
+        return digits_.get_or_prepare(index);
+    }
+
+    /*!
+     * \brief Add a digit to the lowest end.
+     *
+     * \param[in] digit Digit.
+     */
+    void push_to_lowest(digit_type digit) { digits_.push_front(digit); }
+
+    /*!
+     * \brief Add a digit to the highest end.
+     *
+     * \param[in] digit Digit.
+     */
+    void push_to_highest(digit_type digit) { digits_.push_back(digit); }
+
+    /*!
+     * \brief Move digits.
+     *
+     * \param[in] offset Offset from the current places of digits.
+     */
+    void move_digits(index_type offset) { digits_.move_position(offset); }
+
+    /*!
+     * \brief Change the range of digits.
+     *
+     * \param[in] lowest Index of lowest digits.
+     * \param[in] highest Index of the highest digit.
+     */
+    void resize(index_type lowest, index_type highest) {
+        digits_.resize(lowest, highest);
+    }
+
+private:
+    //! Digits.
+    bidirectional_vector<digit_type> digits_;
+};
+
+}  // namespace num_collect::numbers
---
+++ test/units/number/custom_float_test.cpp
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2021 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance using the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Test of custom_float class.
+ */
+#include "num_collect/numbers/custom_float.h"
+
+#include <type_traits>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "num_collect/util/index_type.h"
+
+TEST_CASE("num_collect::numbers::custom_float") {
+    using num_collect::numbers::custom_float;
+
+    constexpr num_collect::index_type radix = 7;
+    using test_type = custom_float<radix>;
+
+    SECTION("copy and move") {
+        STATIC_REQUIRE(std::is_copy_constructible_v<test_type>);
+        STATIC_REQUIRE(std::is_copy_assignable_v<test_type>);
+        STATIC_REQUIRE(std::is_move_constructible_v<test_type>);
+        STATIC_REQUIRE(std::is_move_assignable_v<test_type>);
+    }
+
+    SECTION("construct") {
+        const auto num = test_type();
+
+        REQUIRE(num.lowest_ind() == 0);
+        REQUIRE(num.highest_ind() == 0);
+        REQUIRE(num.at(0) == 0);
+    }
+
+    SECTION("access digits using at") {
+        const auto num = test_type();
+
+        REQUIRE(num.at(0) == 0);
+        REQUIRE_THROWS(num.at(1));
+        REQUIRE_THROWS(num.at(-1));
+    }
+
+    SECTION("access digits using operator[]") {
+        const auto num = test_type();
+
+        REQUIRE(num[0] == 0);
+    }
+
+    SECTION("create digits using operator[]") {
+        auto num = test_type();
+
+        constexpr typename test_type::digit_type digit2 = 5;
+        REQUIRE_NOTHROW(num[2] = digit2);
+
+        REQUIRE(num.lowest_ind() == 0);
+        REQUIRE(num.highest_ind() == 2);
+        REQUIRE(num.at(0) == 0);
+        REQUIRE(num.at(1) == 0);
+        REQUIRE(num.at(2) == digit2);
+    }
+
+    SECTION("create digits using push_to_lowest") {
+        auto num = test_type();
+
+        constexpr typename test_type::digit_type digit1 = 5;
+        REQUIRE_NOTHROW(num.push_to_lowest(digit1));
+
+        REQUIRE(num.lowest_ind() == -1);
+        REQUIRE(num.highest_ind() == 0);
+        REQUIRE(num.at(0) == 0);
+        REQUIRE(num.at(-1) == digit1);
+    }
+
+    SECTION("create digits using push_to_highest") {
+        auto num = test_type();
+
+        constexpr typename test_type::digit_type digit1 = 5;
+        REQUIRE_NOTHROW(num.push_to_highest(digit1));
+
+        REQUIRE(num.lowest_ind() == 0);
+        REQUIRE(num.highest_ind() == 1);
+        REQUIRE(num.at(0) == 0);
+        REQUIRE(num.at(1) == digit1);
+    }
+
+    SECTION("move digits") {
+        auto num = test_type();
+        num[3] = 7;                       // NOLINT
+        num[4] = 11;                      // NOLINT
+        num[5] = 13;                      // NOLINT
+        REQUIRE(num.lowest_ind() == 0);   // NOLINT
+        REQUIRE(num.highest_ind() == 5);  // NOLINT
+
+        REQUIRE_NOTHROW(num.move_digits(-2));
+
+        REQUIRE(num.lowest_ind() == -2);  // NOLINT
+        REQUIRE(num.highest_ind() == 3);  // NOLINT
+        REQUIRE(num.at(1) == 7);          // NOLINT
+        REQUIRE(num.at(2) == 11);         // NOLINT
+        REQUIRE(num.at(3) == 13);         // NOLINT
+    }
+
+    SECTION("change the range of digits") {
+        auto num = test_type();
+        num[3] = 7;                       // NOLINT
+        num[4] = 11;                      // NOLINT
+        num[5] = 13;                      // NOLINT
+        REQUIRE(num.lowest_ind() == 0);   // NOLINT
+        REQUIRE(num.highest_ind() == 5);  // NOLINT
+
+        REQUIRE_NOTHROW(num.resize(-1, 4));  // NOLINT
+        REQUIRE(num.lowest_ind() == -1);     // NOLINT
+        REQUIRE(num.highest_ind() == 4);     // NOLINT
+        REQUIRE(num.at(-1) == 0);            // NOLINT
+        REQUIRE(num.at(0) == 0);             // NOLINT
+        REQUIRE(num.at(1) == 0);             // NOLINT
+        REQUIRE(num.at(2) == 0);             // NOLINT
+        REQUIRE(num.at(3) == 7);             // NOLINT
+        REQUIRE(num.at(4) == 11);            // NOLINT
+    }
+}
