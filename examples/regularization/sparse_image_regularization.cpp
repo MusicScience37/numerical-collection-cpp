2d2246cc830f3dc27ff3bd232f0104ae8b08214f
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-03-30 10:55:46 +0000

    :books: Example of regularization of noizy image using FISTA.

--- examples/regularization/sparse_image_regularization.cpp
+++ examples/regularization/sparse_image_regularization.cpp
@@ -17,11 +17,15 @@
  * \file
  * \brief Example of tikhonov class with blurred sine test problem.
  */
+#include <random>
+
 #include <Eigen/Core>
+#include <Eigen/SparseCore>
 #include <png++/image.hpp>
 #include <png++/rgb_pixel.hpp>
 
 #include "num_collect/base/index_type.h"
+#include "num_collect/regularization/fista.h"
 
 static void add_circle(
     Eigen::MatrixXd& image, const Eigen::Vector2d& center, double radius) {
@@ -39,6 +43,21 @@ static void add_circle(
             if (dist <= radius) {
                 image(i, j) = 1.0;
             }
+        }
+    }
+}
+
+static void add_noise(Eigen::MatrixXd& image, double rate) {
+    std::mt19937 engine;  // NOLINT
+    const double sigma = std::sqrt(
+        image.squaredNorm() / static_cast<double>(image.size()) * rate);
+    std::normal_distribution<double> dist{0.0, sigma};
+
+    const num_collect::index_type rows = image.rows();
+    const num_collect::index_type cols = image.cols();
+    for (num_collect::index_type j = 0; j < cols; ++j) {
+        for (num_collect::index_type i = 0; i < rows; ++i) {
+            image(i, j) += dist(engine);
         }
     }
 }
@@ -70,13 +89,36 @@ static void write_image(
 auto main() -> int {
     constexpr num_collect::index_type rows = 20;
     constexpr num_collect::index_type cols = 20;
-    Eigen::MatrixXd origin = Eigen::MatrixXd::Zero(rows, cols);
+    constexpr num_collect::index_type size = rows * cols;
 
     const Eigen::Vector2d center = Eigen::Vector2d(0.7, 0.6);
     constexpr double radius = 0.2;
+    Eigen::MatrixXd origin = Eigen::MatrixXd::Zero(rows, cols);
     add_circle(origin, center, radius);
-
     write_image(origin, "./sparse_image_origin.png");
+
+    constexpr double noise_rate = 0.1;
+    Eigen::MatrixXd data = origin;
+    add_noise(data, noise_rate);
+    write_image(data, "./sparse_image_data.png");
+
+    using coeff_type = Eigen::SparseMatrix<double>;
+    coeff_type coeff;
+    coeff.resize(size, size);
+    coeff.setIdentity();
+
+    constexpr double reg_param = 0.2;
+    num_collect::regularization::fista<coeff_type, Eigen::VectorXd> solver;
+    const Eigen::VectorXd data_vec = data.reshaped();
+    solver.compute(coeff, data_vec);
+    Eigen::VectorXd solution_vec = data_vec;
+    solver.solve(reg_param, solution_vec);
+
+    const Eigen::MatrixXd solution = solution_vec.reshaped(rows, cols);
+    write_image(solution, "./sparse_image_solution.png");
+
+    const Eigen::MatrixXd error = (solution - origin).cwiseAbs();
+    write_image(error, "./sparse_image_error.png");
 
     return 0;
 }
