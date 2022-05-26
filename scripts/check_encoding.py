5860d0991f00f34933af0085998e6fa81bfb46e0
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-05-26 13:21:18 +0000

    :wrench: Update environment.

--- scripts/check_encoding.py
+++ scripts/check_encoding.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """Check encoding of files.
 """
 
@@ -5,10 +6,10 @@ from sys import argv
 
 
 def check_encoding(filepath: str) -> str:
-    with open(filepath, mode='r', encoding='utf8') as file:
+    with open(filepath, mode="r", encoding="utf8") as file:
         return file.read()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for arg in argv:
         check_encoding(arg)
