655825cfefcecaf85160c25fae29a756cf21ca70
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-05-30 12:35:02 +0000

    :wrench: Fix CI commands.

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -2,9 +2,9 @@
 
 pipenv sync --dev
 
-pipenv run conan profile new --detect default
+pipenv run conan profile new --force --detect default
 pipenv run conan profile update settings.compiler.libcxx=libc++ default
-pipenv run conan remote add gitlab https://gitlab.com/api/v4/packages/conan
+pipenv run conan remote add -f gitlab https://gitlab.com/api/v4/packages/conan
 
 git config --global --add safe.directory /workspaces/numerical-collection-cpp
 pipenv run pre-commit install
