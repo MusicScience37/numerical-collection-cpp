1e28b1875dcaa4b43b20458b32b6a88649011b82
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-07-23 07:02:25 +0000

    :sparkles: Add concept of optimizers.

--- test/units/opt/adaptive_diagonal_curves_test.cpp
+++ test/units/opt/adaptive_diagonal_curves_test.cpp
@@ -26,6 +26,7 @@
 #include <catch2/matchers/catch_matchers_string.hpp>
 
 #include "eigen_approx.h"
+#include "num_collect/opt/concepts/optimizer.h"
 #include "num_prob_collect/opt/multi_quadratic_function.h"
 
 TEST_CASE("num_collect::opt::impl::adc_sample_dict") {
@@ -221,6 +222,11 @@ TEST_CASE("num_collect::opt::impl::adc_group") {
 TEST_CASE("num_collect::opt::adaptive_diagonal_curves") {
     using num_collect::opt::adaptive_diagonal_curves;
     using num_prob_collect::opt::multi_quadratic_function;
+
+    SECTION("concepts") {
+        STATIC_REQUIRE(num_collect::opt::concepts::optimizer<
+            adaptive_diagonal_curves<multi_quadratic_function>>);
+    }
 
     SECTION("init") {
         auto opt = adaptive_diagonal_curves<multi_quadratic_function>();
