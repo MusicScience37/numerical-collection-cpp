4d998c432c369aa7aa2eee03a4beec5f666f4d01
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-08-06 03:10:36 +0000

    :wrench: Output coverage data in lcov's format.

--- scripts/collect_llvm_coverage.sh
+++ scripts/collect_llvm_coverage.sh
@@ -16,6 +16,10 @@ llvm-cov show -ignore-filename-regex='(problems|test|include/num_collect/constan
 llvm-cov show -ignore-filename-regex='(problems|test|include/num_collect/constants)/*' -instr-profile=$DIR/coverage/coverage.profdata $opts \
     -format=html -output-dir=$DIR/coverage/html
 
+# output in lcov's format
+llvm-cov export -ignore-filename-regex='(problems|test|include/num_collect/constants)/*' -instr-profile=$DIR/coverage/coverage.profdata $opts \
+    -format=lcov > $DIR/coverage/coverage
+
 # output summary to stdout
 llvm-cov report -ignore-filename-regex='(problems|test|include/num_collect/constants)/*' -instr-profile=$DIR/coverage/coverage.profdata $opts |
     tee $DIR/coverage/coverage_summary.txt
