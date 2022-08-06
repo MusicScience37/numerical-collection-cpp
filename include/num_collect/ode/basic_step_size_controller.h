a02066f28e1ff64ece23e373e47e4d6a0a45096a
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-08-06 13:45:00 +0000

    :recycle: Applied IWYU to ode module.

--- include/num_collect/ode/basic_step_size_controller.h
+++ include/num_collect/ode/basic_step_size_controller.h
@@ -20,17 +20,15 @@
 #pragma once
 
 #include <cmath>
-#include <optional>
 #include <string_view>
 
-#include "num_collect/base/exception.h"
 #include "num_collect/base/index_type.h"
 #include "num_collect/logging/log_tag_view.h"
-#include "num_collect/logging/logging_mixin.h"
-#include "num_collect/ode/concepts/embedded_formula.h"  // IWYU pragma: keep
-#include "num_collect/ode/error_tolerances.h"
+#include "num_collect/ode/concepts/formula.h"  // IWYU pragma: keep
+#include "num_collect/ode/error_tolerances.h"  // IWYU pragma: keep
 #include "num_collect/ode/impl/get_least_known_order.h"
-#include "num_collect/ode/step_size_limits.h"
+#include "num_collect/ode/step_size_controller_base.h"
+#include "num_collect/ode/step_size_limits.h"  // IWYU pragma: keep
 #include "num_collect/util/assert.h"
 
 namespace num_collect::ode {
@@ -44,38 +42,25 @@ constexpr auto basic_step_size_controller_log_tag =
  *
  * \tparam Formula Type of the formula.
  */
-template <concepts::embedded_formula Formula>
-class basic_step_size_controller : public logging::logging_mixin {
+template <concepts::formula Formula>
+class basic_step_size_controller
+    : public step_size_controller_base<basic_step_size_controller<Formula>,
+          Formula> {
 public:
-    //! Type of formula.
-    using formula_type = Formula;
+    //! Base class.
+    using base_type =
+        step_size_controller_base<basic_step_size_controller<Formula>, Formula>;
 
-    //! Type of problem.
-    using problem_type = typename formula_type::problem_type;
-
-    //! Type of variables.
-    using variable_type = typename problem_type::variable_type;
-
-    //! Type of scalars.
-    using scalar_type = typename problem_type::scalar_type;
+    using typename base_type::formula_type;
+    using typename base_type::problem_type;
+    using typename base_type::scalar_type;
+    using typename base_type::variable_type;
 
     /*!
      * \brief Constructor.
      */
     basic_step_size_controller()
-        : logging::logging_mixin(basic_step_size_controller_log_tag) {}
-
-    /*!
-     * \brief Initialize.
-     *
-     * \param[in] reference Reference variable. (For determining the size of
-     * variables.)
-     */
-    void init(const variable_type& reference) {
-        if (!tolerances_) {
-            tolerances_.emplace(reference);
-        }
-    }
+        : base_type(basic_step_size_controller_log_tag) {}
 
     /*!
      * \brief Check the error estimate and calculate the next step size.
@@ -88,69 +73,11 @@ public:
      */
     [[nodiscard]] auto check_and_calc_next(scalar_type& step_size,
         const variable_type& variable, const variable_type& error) -> bool {
-        if (reduce_if_needed(step_size, variable, error)) {
+        if (this->reduce_if_needed(step_size, variable, error)) {
             return false;
         }
         calc_next(step_size, variable, error);
         return true;
-    }
-
-    /*!
-     * \brief Set the limits of step sizes.
-     *
-     * \param[in] val Value.
-     * \return This.
-     */
-    auto limits(const step_size_limits<scalar_type>& val)
-        -> basic_step_size_controller& {
-        limits_ = val;
-        return *this;
-    }
-
-    /*!
-     * \brief Get the limits of step sizes.
-     *
-     * \return Value.
-     */
-    [[nodiscard]] auto limits() const -> const step_size_limits<scalar_type>& {
-        return limits_;
-    }
-
-    /*!
-     * \brief Set the error tolerances.
-     *
-     * \param[in] val Value.
-     * \return This.
-     */
-    auto tolerances(const error_tolerances<variable_type>& val)
-        -> basic_step_size_controller& {
-        tolerances_ = val;
-        return *this;
-    }
-
-    /*!
-     * \brief Get the error tolerances.
-     *
-     * \return Error tolerances.
-     */
-    [[nodiscard]] auto tolerances() const
-        -> const error_tolerances<variable_type>& {
-        if (!tolerances_) {
-            throw precondition_not_satisfied("Error tolerance is not set yet.");
-        }
-        return *tolerances_;
-    }
-
-    /*!
-     * \brief Set the rate to reduce step sizes when error is large.
-     *
-     * \param[in] val Value.
-     * \return This.
-     */
-    auto reduction_rate(const scalar_type& val) -> basic_step_size_controller& {
-        NUM_COLLECT_ASSERT(val > static_cast<scalar_type>(0));
-        reduction_rate_ = val;
-        return *this;
     }
 
     /*!
@@ -181,35 +108,6 @@ public:
 
 private:
     /*!
-     * \brief Reduce step size if needed.
-     *
-     * \param[in,out] step_size Step size.
-     * \param[in] variable Variable.
-     * \param[in] error Error estimate.
-     * \retval true Step size was reduced.
-     * \retval false Otherwise.
-     */
-    [[nodiscard]] auto reduce_if_needed(scalar_type& step_size,
-        const variable_type& variable, const variable_type& error) -> bool {
-        const bool tolerance_satisfied = tolerances().check(variable, error);
-        if (!tolerance_satisfied) {
-            if (step_size > limits_.lower_limit()) {
-                this->logger().trace()(
-                    "Error tolerance not satisfied with step size {}.",
-                    step_size);
-                step_size *= reduction_rate_;
-                step_size = limits_.apply(step_size);
-                return true;
-            }
-            this->logger().warning()(
-                "Error tolerance not satisfied even with the lowest step size "
-                "{}.",
-                step_size);
-        }
-        return false;
-    }
-
-    /*!
      * \brief Calculate the next step size.
      *
      * \param[in,out] step_size Step size.
@@ -220,7 +118,8 @@ private:
         const variable_type& error) {
         // First, calculate factor of step size using a formula in the
         // reference.
-        const scalar_type error_norm = tolerances_->calc_norm(variable, error);
+        const scalar_type error_norm =
+            this->tolerances().calc_norm(variable, error);
         constexpr index_type order_for_exponent =
             impl::get_least_known_order<Formula>();
         constexpr scalar_type exponent = -static_cast<scalar_type>(1) /
@@ -237,21 +136,8 @@ private:
 
         // Finally, multiply the factor to the step size.
         step_size *= factor;
-        step_size = limits_.apply(step_size);
+        step_size = this->limits().apply(step_size);
     }
-
-    //! Limits of step sizes.
-    step_size_limits<scalar_type> limits_{};
-
-    //! Error tolerances.
-    std::optional<error_tolerances<variable_type>> tolerances_{};
-
-    //! Default rate to reduce step sizes when error is large.
-    static constexpr auto default_reduction_rate =
-        static_cast<scalar_type>(0.5);
-
-    //! Rate to reduce step sizes when error is large.
-    scalar_type reduction_rate_{default_reduction_rate};
 
     //! Default safety coefficient for factors of step sizes.
     static constexpr auto default_step_size_factor_safety_coeff =
--- include/num_collect/ode/pi_step_size_controller.h
+++ include/num_collect/ode/pi_step_size_controller.h
@@ -20,17 +20,15 @@
 #pragma once
 
 #include <cmath>
-#include <optional>
 #include <string_view>
 
-#include "num_collect/base/exception.h"
 #include "num_collect/base/index_type.h"
 #include "num_collect/logging/log_tag_view.h"
-#include "num_collect/logging/logging_mixin.h"
-#include "num_collect/ode/concepts/embedded_formula.h"  // IWYU pragma: keep
-#include "num_collect/ode/error_tolerances.h"
+#include "num_collect/ode/concepts/formula.h"  // IWYU pragma: keep
+#include "num_collect/ode/error_tolerances.h"  // IWYU pragma: keep
 #include "num_collect/ode/impl/get_least_known_order.h"
-#include "num_collect/ode/step_size_limits.h"
+#include "num_collect/ode/step_size_controller_base.h"
+#include "num_collect/ode/step_size_limits.h"  // IWYU pragma: keep
 #include "num_collect/util/assert.h"
 
 namespace num_collect::ode {
@@ -44,20 +42,19 @@ constexpr auto pi_step_size_controller_log_tag =
  *
  * \tparam Formula Type of the formula.
  */
-template <concepts::embedded_formula Formula>
-class pi_step_size_controller : public logging::logging_mixin {
+template <concepts::formula Formula>
+class pi_step_size_controller
+    : public step_size_controller_base<pi_step_size_controller<Formula>,
+          Formula> {
 public:
-    //! Type of formula.
-    using formula_type = Formula;
+    //! Base class.
+    using base_type =
+        step_size_controller_base<pi_step_size_controller<Formula>, Formula>;
 
-    //! Type of problem.
-    using problem_type = typename formula_type::problem_type;
-
-    //! Type of variables.
-    using variable_type = typename problem_type::variable_type;
-
-    //! Type of scalars.
-    using scalar_type = typename problem_type::scalar_type;
+    using typename base_type::formula_type;
+    using typename base_type::problem_type;
+    using typename base_type::scalar_type;
+    using typename base_type::variable_type;
 
     //! Order of the formula used in exponents.
     static constexpr index_type formula_order_for_exponent =
@@ -84,20 +81,7 @@ public:
     /*!
      * \brief Constructor.
      */
-    pi_step_size_controller()
-        : logging::logging_mixin(pi_step_size_controller_log_tag) {}
-
-    /*!
-     * \brief Initialize.
-     *
-     * \param[in] reference Reference variable. (For determining the size of
-     * variables.)
-     */
-    void init(const variable_type& reference) {
-        if (!tolerances_) {
-            tolerances_.emplace(reference);
-        }
-    }
+    pi_step_size_controller() : base_type(pi_step_size_controller_log_tag) {}
 
     /*!
      * \brief Check the error estimate and calculate the next step size.
@@ -110,69 +94,11 @@ public:
      */
     [[nodiscard]] auto check_and_calc_next(scalar_type& step_size,
         const variable_type& variable, const variable_type& error) -> bool {
-        if (reduce_if_needed(step_size, variable, error)) {
+        if (this->reduce_if_needed(step_size, variable, error)) {
             return false;
         }
         calc_next(step_size, variable, error);
         return true;
-    }
-
-    /*!
-     * \brief Set the limits of step sizes.
-     *
-     * \param[in] val Value.
-     * \return This.
-     */
-    auto limits(const step_size_limits<scalar_type>& val)
-        -> pi_step_size_controller& {
-        limits_ = val;
-        return *this;
-    }
-
-    /*!
-     * \brief Get the limits of step sizes.
-     *
-     * \return Value.
-     */
-    [[nodiscard]] auto limits() const -> const step_size_limits<scalar_type>& {
-        return limits_;
-    }
-
-    /*!
-     * \brief Set the error tolerances.
-     *
-     * \param[in] val Value.
-     * \return This.
-     */
-    auto tolerances(const error_tolerances<variable_type>& val)
-        -> pi_step_size_controller& {
-        tolerances_ = val;
-        return *this;
-    }
-
-    /*!
-     * \brief Get the error tolerances.
-     *
-     * \return Error tolerances.
-     */
-    [[nodiscard]] auto tolerances() const
-        -> const error_tolerances<variable_type>& {
-        if (!tolerances_) {
-            throw precondition_not_satisfied("Error tolerance is not set yet.");
-        }
-        return *tolerances_;
-    }
-
-    /*!
-     * \brief Set the rate to reduce step sizes when error is large.
-     *
-     * \param[in] val Value.
-     * \return This.
-     */
-    auto reduction_rate(const scalar_type& val) -> pi_step_size_controller& {
-        NUM_COLLECT_ASSERT(val > static_cast<scalar_type>(0));
-        reduction_rate_ = val;
-        return *this;
     }
 
     /*!
@@ -244,35 +170,6 @@ public:
 
 private:
     /*!
-     * \brief Reduce step size if needed.
-     *
-     * \param[in,out] step_size Step size.
-     * \param[in] variable Variable.
-     * \param[in] error Error estimate.
-     * \retval true Step size was reduced.
-     * \retval false Otherwise.
-     */
-    [[nodiscard]] auto reduce_if_needed(scalar_type& step_size,
-        const variable_type& variable, const variable_type& error) -> bool {
-        const bool tolerance_satisfied = tolerances().check(variable, error);
-        if (!tolerance_satisfied) {
-            if (step_size > limits_.lower_limit()) {
-                this->logger().trace()(
-                    "Error tolerance not satisfied with step size {}.",
-                    step_size);
-                step_size *= reduction_rate_;
-                step_size = limits_.apply(step_size);
-                return true;
-            }
-            this->logger().warning()(
-                "Error tolerance not satisfied even with the lowest step size "
-                "{}.",
-                step_size);
-        }
-        return false;
-    }
-
-    /*!
      * \brief Calculate the next step size.
      *
      * \param[in,out] step_size Step size.
@@ -283,9 +180,8 @@ private:
         const variable_type& error) {
         // First, calculate factor of step size using a formula in the
         // reference.
-        const scalar_type error_norm = tolerances_->calc_norm(variable, error);
-        constexpr index_type order_for_exponent =
-            impl::get_least_known_order<Formula>();
+        const scalar_type error_norm =
+            this->tolerances().calc_norm(variable, error);
         scalar_type factor = pow(error_norm, -current_step_error_exponent_) *
             pow(previous_step_error_, previous_step_error_exponent_);
 
@@ -303,27 +199,14 @@ private:
 
         // Finally, multiply the factor to the step size.
         step_size *= factor;
-        step_size = limits_.apply(step_size);
+        step_size = this->limits().apply(step_size);
 
         // Prepare for the next step.
         previous_step_error_ = error_norm;
     }
 
-    //! Limits of step sizes.
-    step_size_limits<scalar_type> limits_{};
-
-    //! Error tolerances.
-    std::optional<error_tolerances<variable_type>> tolerances_{};
-
     //! Error of the previous time step.
     scalar_type previous_step_error_{static_cast<scalar_type>(1)};
-
-    //! Default rate to reduce step sizes when error is large.
-    static constexpr auto default_reduction_rate =
-        static_cast<scalar_type>(0.5);
-
-    //! Rate to reduce step sizes when error is large.
-    scalar_type reduction_rate_{default_reduction_rate};
 
     //! Exponent of the error of the current time step.
     scalar_type current_step_error_exponent_{
