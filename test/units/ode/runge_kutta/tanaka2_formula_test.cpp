d04fa566866b7c7b58ad4b991d2723e3f8ee8868
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-08-07 08:09:39 +0000

    :white_check_mark: Check of coefficients in Runge-Kutta methods.

--- test/units/ode/runge_kutta/tanaka2_formula_test.cpp
+++ test/units/ode/runge_kutta/tanaka2_formula_test.cpp
@@ -43,6 +43,18 @@ TEST_CASE("num_collect::ode::runge_kutta::tanaka2_formula") {
         STATIC_REQUIRE(formula_type::stages == 3);
         STATIC_REQUIRE(formula_type::order == 4);
         STATIC_REQUIRE(formula_type::lesser_order == 2);
+
+        CHECK_THAT(
+            formula_type::a11, Catch::Matchers::WithinRel(formula_type::b1));
+        CHECK_THAT(formula_type::a21 + formula_type::a22,
+            Catch::Matchers::WithinRel(formula_type::b2));
+        CHECK_THAT(formula_type::a31 + formula_type::a32 + formula_type::a33,
+            Catch::Matchers::WithinRel(formula_type::b3));
+        CHECK_THAT(formula_type::c1 + formula_type::c2 + formula_type::c3,
+            Catch::Matchers::WithinRel(1.0));
+        CHECK_THAT(formula_type::cw2, Catch::Matchers::WithinRel(1.0));
+        CHECK_THAT(formula_type::ce1 + formula_type::ce2 + formula_type::ce3,
+            Catch::Matchers::WithinAbs(0.0, 1e-10));  // NOLINT
     }
 
     SECTION("initialize") {
