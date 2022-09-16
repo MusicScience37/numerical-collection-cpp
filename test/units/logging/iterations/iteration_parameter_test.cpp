02ac7c7627458b590bca59fe59fe0cdc8022c4f8
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-09-16 14:06:24 +0000

    :white_check_mark: Add tests of iteration_parameter class for other patterns.

--- test/units/logging/iterations/iteration_parameter_test.cpp
+++ test/units/logging/iterations/iteration_parameter_test.cpp
@@ -24,11 +24,19 @@
 #include <catch2/catch_test_macros.hpp>
 #include <fmt/format.h>
 
+#include "mock_algorithm.h"
+#include "num_collect/logging/iterations/member_function_iteration_parameter_value.h"
+#include "num_collect/logging/iterations/member_variable_iteration_parameter_value.h"
 #include "num_collect/logging/iterations/variable_iteration_parameter_value.h"
 
 TEST_CASE("num_collect::logging::iterations::iteration_parameter") {
     using num_collect::logging::iterations::iteration_parameter;
+    using num_collect::logging::iterations::
+        member_function_iteration_parameter_value;
+    using num_collect::logging::iterations::
+        member_variable_iteration_parameter_value;
     using num_collect::logging::iterations::variable_iteration_parameter_value;
+    using num_collect_test::logging::iterations::mock_algorithm;
 
     SECTION("use variable_iteration_parameter_value") {
         using algorithm_type = void;
@@ -63,6 +71,80 @@ TEST_CASE("num_collect::logging::iterations::iteration_parameter") {
 
         buffer.clear();
         CHECK_NOTHROW(parameter.format_summary_to(buffer, algorithm));
+        CHECK(std::string(buffer.data(), buffer.size()) == "abc=12345");
+    }
+
+    SECTION("use member_variable_iteration_parameter_value") {
+        using value_type = int;
+        using algorithm_type = mock_algorithm<value_type, value_type>;
+        using parameter_value_type =
+            member_variable_iteration_parameter_value<algorithm_type,
+                value_type>;
+        using parameter_type = iteration_parameter<algorithm_type, value_type,
+            parameter_value_type>;
+
+        const std::string label = "abc";
+        algorithm_type algorithm{12345};  // NOLINT
+        constexpr num_collect::index_type width = 7;
+        parameter_type parameter{
+            label, parameter_value_type{&algorithm_type::value1}};
+        parameter.width(width);
+
+        fmt::memory_buffer buffer;
+        CHECK_NOTHROW(parameter.format_label_to(buffer));
+        CHECK(std::string(buffer.data(), buffer.size()) == "    abc");
+
+        buffer.clear();
+        CHECK_THROWS(parameter.format_value_to(buffer));
+
+        buffer.clear();
+        CHECK_THROWS(parameter.format_summary_to(buffer));
+
+        buffer.clear();
+        CHECK_NOTHROW(parameter.format_value_to(buffer, &algorithm));
+        CHECK(std::string(buffer.data(), buffer.size()) == "  12345");
+
+        buffer.clear();
+        CHECK_NOTHROW(parameter.format_summary_to(buffer, &algorithm));
+        CHECK(std::string(buffer.data(), buffer.size()) == "abc=12345");
+    }
+
+    SECTION("use member_function_iteration_parameter_value") {
+        using value_type = int;
+        using return_type = value_type;
+        using algorithm_type = mock_algorithm<value_type, return_type>;
+        using parameter_value_type =
+            member_function_iteration_parameter_value<algorithm_type,
+                value_type, return_type>;
+        using parameter_type = iteration_parameter<algorithm_type, value_type,
+            parameter_value_type>;
+
+        const std::string label = "abc";
+        value_type value = 12345;     // NOLINT
+        algorithm_type algorithm{0};  // NOLINT
+        // NOLINTNEXTLINE
+        ALLOW_CALL(algorithm, get_impl()).RETURN(value);
+        constexpr num_collect::index_type width = 7;
+        parameter_type parameter{
+            label, parameter_value_type{&algorithm_type::get}};
+        parameter.width(width);
+
+        fmt::memory_buffer buffer;
+        CHECK_NOTHROW(parameter.format_label_to(buffer));
+        CHECK(std::string(buffer.data(), buffer.size()) == "    abc");
+
+        buffer.clear();
+        CHECK_THROWS(parameter.format_value_to(buffer));
+
+        buffer.clear();
+        CHECK_THROWS(parameter.format_summary_to(buffer));
+
+        buffer.clear();
+        CHECK_NOTHROW(parameter.format_value_to(buffer, &algorithm));
+        CHECK(std::string(buffer.data(), buffer.size()) == "  12345");
+
+        buffer.clear();
+        CHECK_NOTHROW(parameter.format_summary_to(buffer, &algorithm));
         CHECK(std::string(buffer.data(), buffer.size()) == "abc=12345");
     }
 }
