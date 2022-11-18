849b70aafa0e05942aff4008513593dd302286c2
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-11-18 15:56:18 +0000

    :recycle: Refactor schema using "if".

--- config_schema/tests/conftest.py
+++ config_schema/tests/conftest.py
@@ -10,10 +10,14 @@ from config_validator import ConfigValidator
 @pytest.fixture(
     scope="session",
     params=[
-        "https://json-schema.org/draft-04/schema",
-        None,
+        ("config_schema4.json", "https://json-schema.org/draft-04/schema"),
+        ("config_schema4.json", None),
+        ("config_schema7.json", None),
     ],
 )
 def validator(request: pytest.FixtureRequest) -> ConfigValidator:
     """Get the validator."""
-    return ConfigValidator(meta_schema_url=request.param)
+    schema_filename, meta_schema_url = request.param
+    return ConfigValidator(
+        schema_filename=schema_filename, meta_schema_url=meta_schema_url
+    )
