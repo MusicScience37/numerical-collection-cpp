8475aaf649544154203b45b3de5f76d5043aff71
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-11-19 12:16:36 +0000

    Update Conan packages.

--- conanfile.py
+++ conanfile.py
@@ -23,22 +23,22 @@ class NumCollectConan(ConanFile):
     generators = "cmake", "cmake_find_package"
 
     def requirements(self):
-        self.requires("fmt/9.0.0")
+        self.requires("fmt/9.1.0")
         self.requires("eigen/3.4.0")
         self.requires("cpp_hash_tables/0.3.0@MusicScience37+cpp-hash-tables/stable")
-        self.requires("tomlplusplus/3.1.0")
+        self.requires("tomlplusplus/3.2.0")
 
     def build_requirements(self):
         if self.options.requirements_for_tests:
             self.build_requires("catch2/3.1.0")
-            self.build_requires("trompeloeil/42")
+            self.build_requires("trompeloeil/43")
             self.build_requires(
-                "cpp_stat_bench/0.7.0@MusicScience37+cpp-stat-bench/stable"
+                "cpp_stat_bench/0.8.0@MusicScience37+cpp-stat-bench/stable"
             )
             self.build_requires("pybind11/2.10.0")
             self.build_requires("approvaltests.cpp/10.12.2")
             self.build_requires("pngpp/0.2.10")
-            self.build_requires("lyra/1.6.0")
+            self.build_requires("lyra/1.6.1")
 
     def package(self):
         self.copy("*.h")
