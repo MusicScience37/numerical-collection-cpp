081fd7629d87a5d38fc017b3ee3913608bc26b46
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-02-17 14:31:43 +0000

    :wrench: Update environment.

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git config --global --add safe.directory $(pwd)
+
 poetry config virtualenvs.in-project true
 poetry env use 3.10
 poetry install
@@ -9,7 +11,6 @@ poetry run conan profile update settings.compiler.libcxx=libc++ default
 poetry run conan remote add -f cpp-stat-bench https://gitlab.com/api/v4/projects/32226502/packages/conan
 poetry run conan remote add -f cpp-hash-tables https://gitlab.com/api/v4/projects/35726343/packages/conan
 
-git config --global --add safe.directory /workspaces/numerical-collection-cpp
 poetry run pre-commit install
 
 git config commit.template .gitmessage
