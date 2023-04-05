a56c68f66a0aa217720ab14326a6bb0f71cfdd83
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-04-05 22:33:36 +0900

    :wrench: Use vcpkg to install dependencies.

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -6,11 +6,6 @@ poetry config virtualenvs.in-project true
 poetry env use 3.10
 poetry install
 
-poetry run conan profile new --force --detect default
-poetry run conan profile update settings.compiler.libcxx=libc++ default
-poetry run conan remote add -f cpp-stat-bench https://gitlab.com/api/v4/projects/32226502/packages/conan
-poetry run conan remote add -f cpp-hash-tables https://gitlab.com/api/v4/projects/35726343/packages/conan
-
 poetry run pre-commit install
 
 git config commit.template .gitmessage
