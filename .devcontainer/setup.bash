ee0848eafc37328cabe61aa856a5f65253cd568b
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-04-14 15:56:44 +0000

    :wrench: Updated libraries.

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -3,7 +3,7 @@
 git config --global --add safe.directory $(pwd)
 
 poetry config virtualenvs.in-project true
-poetry env use 3.10
+poetry env use 3.11
 poetry install
 
 poetry run pre-commit install
