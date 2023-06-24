cb35159603e11af0379f4f1ea84171e5f8ceb48c
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-06-24 12:35:45 +0000

    :white_check_mark: Add test of vibration with external forces.

--- test/integ/ode_comb/source_list.cmake
+++ test/integ/ode_comb/source_list.cmake
@@ -1,6 +1,8 @@
 set(SOURCE_FILES
     exponential/embedded.cpp
     exponential/non_embedded.cpp
+    external_force_vibration_problem.h/embedded.cpp
+    external_force_vibration_problem.h/non_embedded.cpp
     implicit_exponential/embedded.cpp
     implicit_kaps/embedded.cpp
     spring_movement/embedded.cpp
--- test/integ/ode_comb/unity_source.cpp
+++ test/integ/ode_comb/unity_source.cpp
@@ -1,5 +1,7 @@
 #include "exponential/embedded.cpp"      // NOLINT(bugprone-suspicious-include)
 #include "exponential/non_embedded.cpp"  // NOLINT(bugprone-suspicious-include)
+#include "external_force_vibration_problem.h/embedded.cpp"  // NOLINT(bugprone-suspicious-include)
+#include "external_force_vibration_problem.h/non_embedded.cpp"  // NOLINT(bugprone-suspicious-include)
 #include "implicit_exponential/embedded.cpp"  // NOLINT(bugprone-suspicious-include)
 #include "implicit_kaps/embedded.cpp"    // NOLINT(bugprone-suspicious-include)
 #include "spring_movement/embedded.cpp"  // NOLINT(bugprone-suspicious-include)
