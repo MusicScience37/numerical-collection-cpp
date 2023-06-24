4e4c5cd366baa40ac0e4acbd76b0fdf70b43ae41
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-06-24 10:42:48 +0000

    :white_check_mark: Add tests of some implicit problems.

--- test/integ/ode_comb/source_list.cmake
+++ test/integ/ode_comb/source_list.cmake
@@ -1,6 +1,8 @@
 set(SOURCE_FILES
     exponential/embedded.cpp
     exponential/non_embedded.cpp
+    implicit_exponential/embedded.cpp
+    implicit_kaps/embedded.cpp
     spring_movement/embedded.cpp
     spring_movement/non_embedded.cpp
 )
--- test/integ/ode_comb/unity_source.cpp
+++ test/integ/ode_comb/unity_source.cpp
@@ -1,4 +1,6 @@
 #include "exponential/embedded.cpp"      // NOLINT(bugprone-suspicious-include)
 #include "exponential/non_embedded.cpp"  // NOLINT(bugprone-suspicious-include)
+#include "implicit_exponential/embedded.cpp"  // NOLINT(bugprone-suspicious-include)
+#include "implicit_kaps/embedded.cpp"    // NOLINT(bugprone-suspicious-include)
 #include "spring_movement/embedded.cpp"  // NOLINT(bugprone-suspicious-include)
 #include "spring_movement/non_embedded.cpp"  // NOLINT(bugprone-suspicious-include)
