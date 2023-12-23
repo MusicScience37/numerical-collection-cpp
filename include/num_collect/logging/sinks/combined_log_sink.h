1303747053c5b7bd074993e7d363ca4b49585b11
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-12-23 16:25:57 +0000

    :recycle: Change the place to implement combined_log_sink to prevent unstable ABI.

--- include/num_collect/logging/sinks/combined_log_sink.h
+++ include/num_collect/logging/sinks/combined_log_sink.h
@@ -19,14 +19,67 @@
  */
 #pragma once
 
+#include <string_view>
 #include <utility>
 #include <vector>
 
-#include "num_collect/impl/num_collect_export.h"
 #include "num_collect/logging/log_level.h"
 #include "num_collect/logging/sinks/log_sink.h"
+#include "num_collect/logging/time_stamp.h"
+#include "num_collect/util/source_info_view.h"
 
 namespace num_collect::logging::sinks {
+
+/*!
+ * \brief Class of log sinks to write logs to multiple log sinks.
+ *
+ * \thread_safety Thread-safe for all operations.
+ */
+class combined_log_sink {
+public:
+    /*!
+     * \brief Constructor.
+     *
+     * \param[in] sinks Log sinks with log levels.
+     */
+    explicit combined_log_sink(
+        std::vector<std::pair<log_sink, log_level>> sinks)
+        : sinks_(std::move(sinks)) {}
+
+    /*!
+     * \brief Destructor.
+     */
+    ~combined_log_sink() = default;
+
+    combined_log_sink(const combined_log_sink&) = delete;
+    combined_log_sink(combined_log_sink&&) = delete;
+    auto operator=(const combined_log_sink&) -> combined_log_sink& = delete;
+    auto operator=(combined_log_sink&&) -> combined_log_sink& = delete;
+
+    /*!
+     * \brief Write a log.
+     *
+     * \param[in] time Time.
+     * \param[in] tag Tag.
+     * \param[in] level Log level.
+     * \param[in] source Information of the source code.
+     * \param[in] body Log body.
+     *
+     * \note This function can be called from multiple threads.
+     */
+    void write(time_stamp time, std::string_view tag, log_level level,
+        util::source_info_view source, std::string_view body) noexcept {
+        for (const auto& [sink, output_log_level] : sinks_) {
+            if (level >= output_log_level) {
+                sink.write(time, tag, level, source, body);
+            }
+        }
+    }
+
+private:
+    //! Log sinks with log levels.
+    std::vector<std::pair<log_sink, log_level>> sinks_;
+};
 
 /*!
  * \brief Create a log sink to write logs to multiple log sinks.
@@ -34,7 +87,9 @@ namespace num_collect::logging::sinks {
  * \param[in] sinks Log sinks with log levels.
  * \return Log sink to write logs to all of the given log sinks.
  */
-[[nodiscard]] NUM_COLLECT_EXPORT auto create_combined_log_sink(
-    std::vector<std::pair<log_sink, log_level>> sinks) -> log_sink;
+[[nodiscard]] inline auto create_combined_log_sink(
+    std::vector<std::pair<log_sink, log_level>> sinks) -> log_sink {
+    return create_log_sink<combined_log_sink>(std::move(sinks));
+}
 
 }  // namespace num_collect::logging::sinks
