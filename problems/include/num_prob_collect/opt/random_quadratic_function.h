cf40b4242bfbf0857d91c68382b27379f65528ba
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2023-12-24 02:33:42 +0000

    :alembic: Implemented a benchmark of single-variate optimization using random quadratic functions.

---
+++ problems/include/num_prob_collect/opt/random_quadratic_function.h
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2023 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of random_quadratic_function class.
+ */
+#pragma once
+
+#include <random>
+
+namespace num_prob_collect::opt {
+
+/*!
+ * \brief Class of quadratic functions with random coefficients and optimal
+ * variables.
+ */
+class random_quadratic_function {
+public:
+    //! Type of variables.
+    using variable_type = double;
+    //! Type of function values.
+    using value_type = double;
+
+    /*!
+     * \brief Constructor.
+     *
+     * \param[in] coeff Coefficient.
+     * \param[in] optimal_variable Optimal variable.
+     */
+    random_quadratic_function(double coeff, double optimal_variable) noexcept
+        : coeff_(coeff), optimal_variable_(optimal_variable) {}
+
+    /*!
+     * \brief Evaluate function value on variable.
+     *
+     * \param[in] variable Variable.
+     */
+    void evaluate_on(double variable) noexcept {
+        const double diff = variable - optimal_variable_;
+        value_ = coeff_ * diff * diff;
+    }
+
+    /*!
+     * \brief Get function value.
+     *
+     * \return Function value.
+     */
+    [[nodiscard]] auto value() const noexcept -> const double& {
+        return value_;
+    }
+
+    /*!
+     * \brief Get the coefficient.
+     *
+     * \return Coefficient.
+     */
+    [[nodiscard]] auto coeff() const noexcept -> double { return coeff_; }
+
+    /*!
+     * \brief Get the optimal variable.
+     *
+     * \return Optimal variable.
+     */
+    [[nodiscard]] auto optimal_variable() const noexcept -> double {
+        return optimal_variable_;
+    }
+
+private:
+    //! Coefficient.
+    double coeff_;
+
+    //! Optimal variable.
+    double optimal_variable_;
+
+    //! Function value.
+    double value_{0.0};
+};
+
+/*!
+ * \brief Class to generate random quadratic functions.
+ */
+class random_quadratic_function_generator {
+public:
+    //! Maximum optimal variable.
+    static constexpr double min_variable = -10.0;
+
+    //! Minimum optimal variable.
+    static constexpr double max_variable = 10.0;
+
+    /*!
+     * \brief Constructor.
+     */
+    random_quadratic_function_generator() noexcept = default;
+
+    /*!
+     * \brief Generate a random quadratic function.
+     *
+     * \return Generated quadratic function.
+     */
+    [[nodiscard]] auto operator()() noexcept -> random_quadratic_function {
+        const double coeff = coeff_dist_(generator_);
+        const double optimal_variable = optimal_variable_dist_(generator_);
+        return random_quadratic_function(coeff, optimal_variable);
+    }
+
+private:
+    //! Random number generator.
+    std::mt19937
+        generator_{};  // NOLINT(cert-msc32-c,cert-msc51-cpp): For reproducibility.
+
+    //! Distribution of coefficients.
+    std::uniform_real_distribution<double> coeff_dist_{0.5, 3.0};  // NOLINT
+
+    //! Distribution of optimal variables.
+    std::uniform_real_distribution<double> optimal_variable_dist_{
+        min_variable, max_variable};
+};
+
+}  // namespace num_prob_collect::opt
---
+++ test/bench/opt/random_quadratic_function_test.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2023 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Benchmark of random quadratic functions.
+ */
+#include "num_prob_collect/opt/random_quadratic_function.h"
+
+#include <algorithm>
+#include <iterator>
+#include <utility>
+#include <vector>
+
+#include <stat_bench/benchmark_macros.h>
+#include <stat_bench/invocation_context.h>
+#include <stat_bench/stat/custom_stat_output.h>
+
+#include "num_collect/base/index_type.h"
+#include "num_collect/opt/dividing_rectangles.h"
+#include "num_collect/opt/golden_section_search.h"
+#include "num_collect/opt/heuristic_global_optimizer.h"
+#include "num_collect/opt/sampling_optimizer.h"
+
+STAT_BENCH_MAIN
+
+class random_quadratic_function_fixture : public stat_bench::FixtureBase {
+public:
+    random_quadratic_function_fixture() = default;
+
+    template <typename Optimizer>
+    void test_optimizer(std::size_t sample_index, Optimizer& optimizer) {
+        constexpr double tol_value = 1e-3;
+        while (optimizer.opt_value() > tol_value) {
+            optimizer.iterate();
+        }
+        constexpr std::size_t thread_index = 0;
+        iterations_stat_->add(
+            thread_index, sample_index, optimizer.iterations());
+        evaluations_stat_->add(
+            thread_index, sample_index, optimizer.evaluations());
+    }
+
+    void setup(stat_bench::InvocationContext& context) override {
+        const std::size_t num_samples = context.samples();
+        functions_.reserve(num_samples);
+        functions_.clear();
+        std::generate_n(std::back_inserter(functions_), num_samples,
+            num_prob_collect::opt::random_quadratic_function_generator());
+
+        iterations_stat_ = context.add_custom_stat("iterations");
+        evaluations_stat_ = context.add_custom_stat("evaluations");
+    }
+
+    [[nodiscard]] auto function(std::size_t i) const
+        -> const num_prob_collect::opt::random_quadratic_function& {
+        return functions_.at(i);
+    }
+
+    [[nodiscard]] static auto search_region() -> std::pair<double, double> {
+        return {num_prob_collect::opt::random_quadratic_function_generator::
+                    min_variable,
+            num_prob_collect::opt::random_quadratic_function_generator::
+                max_variable};
+    }
+
+private:
+    std::vector<num_prob_collect::opt::random_quadratic_function> functions_;
+
+    std::shared_ptr<stat_bench::stat::CustomStatOutput> iterations_stat_;
+
+    std::shared_ptr<stat_bench::stat::CustomStatOutput> evaluations_stat_;
+};
+
+// NOLINTNEXTLINE
+STAT_BENCH_CASE_F(random_quadratic_function_fixture,
+    "opt_random_quadratic_function", "golden_section_search") {
+    STAT_BENCH_MEASURE_INDEXED(
+        /*thread_index*/, sample_index, /*iteration_index*/) {
+        auto optimizer = num_collect::opt::golden_section_search<
+            num_prob_collect::opt::random_quadratic_function>(
+            this->function(sample_index));
+        const auto [lower, upper] = search_region();
+        optimizer.init(lower, upper);
+        this->test_optimizer(sample_index, optimizer);
+    };
+}
+
+// NOLINTNEXTLINE
+STAT_BENCH_CASE_F(random_quadratic_function_fixture,
+    "opt_random_quadratic_function", "dividing_rectangles") {
+    STAT_BENCH_MEASURE_INDEXED(
+        /*thread_index*/, sample_index, /*iteration_index*/) {
+        auto optimizer = num_collect::opt::dividing_rectangles<
+            num_prob_collect::opt::random_quadratic_function>(
+            this->function(sample_index));
+        const auto [lower, upper] = search_region();
+        optimizer.init(lower, upper);
+        this->test_optimizer(sample_index, optimizer);
+    };
+}
+
+// NOLINTNEXTLINE
+STAT_BENCH_CASE_F(random_quadratic_function_fixture,
+    "opt_random_quadratic_function", "sampling_optimizer") {
+    STAT_BENCH_MEASURE_INDEXED(
+        /*thread_index*/, sample_index, /*iteration_index*/) {
+        auto optimizer = num_collect::opt::sampling_optimizer<
+            num_prob_collect::opt::random_quadratic_function>(
+            this->function(sample_index));
+        const auto [lower, upper] = search_region();
+        optimizer.init(lower, upper);
+        this->test_optimizer(sample_index, optimizer);
+    };
+}
+
+// NOLINTNEXTLINE
+STAT_BENCH_CASE_F(random_quadratic_function_fixture,
+    "opt_random_quadratic_function", "heuristic_global_optimizer") {
+    STAT_BENCH_MEASURE_INDEXED(
+        /*thread_index*/, sample_index, /*iteration_index*/) {
+        auto optimizer = num_collect::opt::heuristic_global_optimizer<
+            num_prob_collect::opt::random_quadratic_function>(
+            this->function(sample_index));
+        const auto [lower, upper] = search_region();
+        optimizer.init(lower, upper);
+        this->test_optimizer(sample_index, optimizer);
+    };
+}
