f29eb91fc301d666df85c93988d5ec8e2447ee0e
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-01-11 15:21:22 +0000

    :construction: Add commands to configure using CMake and MSVC.

---
+++ scripts/ci_win.cmd
@@ -0,0 +1,15 @@
+call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\VC\\Auxiliary\\Build\\vcvarsall.bat" x86_x64
+
+cmake .. ^
+    -G Ninja ^
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ^
+    -DCMAKE_TOOLCHAIN_FILE=..\vcpkg\scripts\buildsystems\vcpkg.cmake ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DNUM_COLLECT_TESTING:BOOL=ON ^
+    -DNUM_COLLECT_ENABLE_UNIT_TESTS=ON ^
+    -DNUM_COLLECT_ENABLE_INTEG_TESTS=ON ^
+    -DNUM_COLLECT_ENABLE_BENCH=OFF ^
+    -DNUM_COLLECT_TEST_BENCHMARKS=OFF ^
+    -DNUM_COLLECT_BUILD_EXAMPLES=OFF ^
+    -DNUM_COLLECT_WRITE_JUNIT:BOOL=ON ^
+    -DBUILD_SHARED_LIBS:BOOL=ON
