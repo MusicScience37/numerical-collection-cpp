da6f764fa1bf11d356183927d8065a44bb995f6b
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-05-01 05:23:41 +0000

    :white_check_mark: Shorter test name is required in MSVC.

--- test/integ/rbf_interp_comb/source_list.cmake
+++ test/integ/rbf_interp_comb/source_list.cmake
@@ -1,3 +1,3 @@
 set(SOURCE_FILES
-    rbfs/test_global_exact_rbf_interpolator.cpp
+    rbfs/global_exact_rbf_interpolator.cpp
 )
--- test/integ/rbf_interp_comb/unity_source.cpp
+++ test/integ/rbf_interp_comb/unity_source.cpp
@@ -1,1 +1,1 @@
-#include "rbfs/test_global_exact_rbf_interpolator.cpp"  // NOLINT(bugprone-suspicious-include)
+#include "rbfs/global_exact_rbf_interpolator.cpp"  // NOLINT(bugprone-suspicious-include)
