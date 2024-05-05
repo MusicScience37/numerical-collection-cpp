8c9fd1609175f0abbf2474dedc36ef509db03d7c
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-05-05 09:56:48 +0000

    :white_check_mark: Test of rbf_polynomial_interpolator for 2D.

--- test/units/rbf/rbf_polynomial_interpolator_test.cpp
+++ test/units/rbf/rbf_polynomial_interpolator_test.cpp
@@ -29,7 +29,9 @@
 #include "num_collect/constants/pi.h"
 #include "num_collect/rbf/rbfs/gaussian_rbf.h"
 
-TEST_CASE("num_collect::rbf::global_rbf_polynomial_interpolator") {
+TEST_CASE(
+    "num_collect::rbf::global_rbf_polynomial_interpolator "
+    "(for scalar variable)") {
     using num_collect::rbf::global_rbf_polynomial_interpolator;
     using num_collect::rbf::rbfs::gaussian_rbf;
 
@@ -67,6 +69,68 @@ TEST_CASE("num_collect::rbf::global_rbf_polynomial_interpolator") {
             interpolated_values(i) =
                 interpolator.interpolate(interpolated_variables(i));
             actual_values(i) = function(interpolated_variables(i));
+        }
+        comparison_approvals::verify_with_reference(
+            interpolated_values, actual_values, 2);
+    }
+}
+
+TEST_CASE(
+    "num_collect::rbf::global_rbf_polynomial_interpolator "
+    "(for vector variable)") {
+    using num_collect::rbf::global_rbf_polynomial_interpolator;
+    using num_collect::rbf::rbfs::gaussian_rbf;
+
+    using rbf_interpolator_type =
+        global_rbf_polynomial_interpolator<double(Eigen::Vector2d)>;
+
+    rbf_interpolator_type interpolator;
+
+    const auto function = [](const Eigen::Vector2d& var) {
+        return var.squaredNorm();
+    };
+
+    std::vector<Eigen::Vector2d> sample_variables;
+    for (std::size_t i = 0; i < 3; ++i) {
+        const double x = static_cast<double>(i) / static_cast<double>(2);
+        for (std::size_t j = 0; j < 3; ++j) {
+            const double y = static_cast<double>(j) / static_cast<double>(2);
+            sample_variables.emplace_back(x, y);
+        }
+    }
+    Eigen::VectorXd sample_values{};
+    sample_values.resize(
+        static_cast<num_collect::index_type>(sample_variables.size()));
+    for (std::size_t i = 0; i < sample_variables.size(); ++i) {
+        sample_values(static_cast<num_collect::index_type>(i)) =
+            function(sample_variables[i]);
+    }
+
+    std::vector<Eigen::Vector2d> interpolated_variables;
+    for (std::size_t i = 0; i < 5; ++i) {  // NOLINT
+        const double x = static_cast<double>(i) / static_cast<double>(4);
+        for (std::size_t j = 0; j < 5; ++j) {  // NOLINT
+            const double y = static_cast<double>(j) / static_cast<double>(4);
+            interpolated_variables.emplace_back(x, y);
+        }
+    }
+
+    SECTION("interpolate with a fixed scale") {
+        constexpr double length_parameter_scale = 2.0;
+        interpolator.fix_length_parameter_scale(length_parameter_scale);
+        interpolator.compute(sample_variables, sample_values);
+
+        Eigen::VectorXd interpolated_values;
+        interpolated_values.resize(static_cast<num_collect::index_type>(
+            interpolated_variables.size()));
+        Eigen::VectorXd actual_values;
+        actual_values.resize(static_cast<num_collect::index_type>(
+            interpolated_variables.size()));
+        for (std::size_t i = 0; i < interpolated_variables.size(); ++i) {
+            interpolated_values(static_cast<num_collect::index_type>(i)) =
+                interpolator.interpolate(interpolated_variables[i]);
+            actual_values(static_cast<num_collect::index_type>(i)) =
+                function(interpolated_variables[i]);
         }
         comparison_approvals::verify_with_reference(
             interpolated_values, actual_values, 2);
