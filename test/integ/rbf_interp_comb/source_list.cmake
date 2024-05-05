c33da4929c5936ddefd87553cf2aa0eebbe0f3e7
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-05-05 10:07:44 +0000

    :white_check_mark: Integration tests and benchmarks of the new class.

--- test/integ/rbf_interp_comb/source_list.cmake
+++ test/integ/rbf_interp_comb/source_list.cmake
@@ -1,5 +1,6 @@
 set(SOURCE_FILES
     rbfs/gaussian_process_interpolator.cpp
+    rbfs/global_rbf_polynomial_interpolator.cpp
     rbfs/local_csrbf_interpolator.cpp
     rbfs/local_rbf_interpolator.cpp
 )
