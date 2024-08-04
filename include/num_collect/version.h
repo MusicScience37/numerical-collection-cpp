c0683fc2b2b2d4d23b601d964c3da19bafb3e259
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-08-04 10:36:20 +0000

    :books: Prepare for release v0.10.0.

--- include/num_collect/version.h
+++ include/num_collect/version.h
@@ -23,7 +23,7 @@
 #define NUM_COLLECT_VERSION_MAJOR 0
 
 //! Minor version of numerical-collection-cpp project.
-#define NUM_COLLECT_VERSION_MINOR 9
+#define NUM_COLLECT_VERSION_MINOR 10
 
 //! Patch version of numerical-collection-cpp project.
 #define NUM_COLLECT_VERSION_PATCH 0
