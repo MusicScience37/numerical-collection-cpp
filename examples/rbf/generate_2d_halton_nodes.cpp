b1b9b5964675e143b3ada970f0a2784f787a1ddd
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-05-11 10:19:42 +0000

    :recycle: Change plotting of an example.

--- examples/rbf/generate_2d_halton_nodes.cpp
+++ examples/rbf/generate_2d_halton_nodes.cpp
@@ -17,64 +17,52 @@
  * \file
  * \brief Example to generate Halton nodes in 2D.
  */
-#include <exception>
 #include <iostream>
 #include <string>
-#include <unordered_map>
-#include <vector>
 
 #include <Eigen/Core>
 #include <fmt/format.h>
 #include <lyra/lyra.hpp>
-#include <pybind11/embed.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>  // IWYU pragma: keep
+#include <plotly_plotter/data_table.h>
+#include <plotly_plotter/figure_builders/scatter.h>
+#include <plotly_plotter/write_html.h>
+#include <plotly_plotter/write_png.h>
 
 #include "num_collect/base/index_type.h"
 #include "num_collect/rbf/generate_halton_nodes.h"
 
 auto main(int argc, char** argv) -> int {
-    pybind11::scoped_interpreter interpreter;
-    try {
-        num_collect::index_type num_nodes = 200;  // NOLINT
-        const auto cli = lyra::cli().add_argument(lyra::opt(num_nodes, "Number")
-                .name("--num_nodes")
-                .name("-n")
-                .optional()
-                .help("Set the number of nodes."));
-        const auto result = cli.parse({argc, argv});
-        if (!result) {
-            std::cerr << result.message() << "\n\n";
-            std::cerr << cli << std::endl;  // NOLINT
-            return 1;
-        }
-
-        const auto nodes =
-            num_collect::rbf::generate_halton_nodes<double, 2>(num_nodes);
-
-        std::vector<double> x_list;
-        std::vector<double> y_list;
-        x_list.reserve(nodes.size());
-        y_list.reserve(nodes.size());
-        for (const auto& node : nodes) {
-            x_list.push_back(node.x());
-            y_list.push_back(node.y());
-        }
-        std::unordered_map<std::string, pybind11::object> data;
-        data.try_emplace("x", pybind11::cast(x_list));
-        data.try_emplace("y", pybind11::cast(y_list));
-
-        pybind11::module::import("pandas");
-        auto px = pybind11::module::import("plotly.express");
-        auto fig = px.attr("scatter")(pybind11::arg("data_frame") = data,
-            pybind11::arg("x") = "x", pybind11::arg("y") = "y",
-            pybind11::arg("title") =
-                fmt::format("{} Halton nodes in 2D", num_nodes));
-
-        fig.attr("write_html")("2d_halton_nodes.html");
-        fig.attr("write_image")("2d_halton_nodes.png");
-    } catch (const std::exception& e) {
-        std::cerr << "Exception thrown: " << e.what() << std::endl;  // NOLINT
+    num_collect::index_type num_nodes = 200;  // NOLINT
+    const auto cli = lyra::cli().add_argument(lyra::opt(num_nodes, "Number")
+            .name("--num_nodes")
+            .name("-n")
+            .optional()
+            .help("Set the number of nodes."));
+    const auto result = cli.parse({argc, argv});
+    if (!result) {
+        std::cerr << result.message() << "\n\n";
+        std::cerr << cli << std::endl;  // NOLINT
         return 1;
     }
+
+    const auto nodes =
+        num_collect::rbf::generate_halton_nodes<double, 2>(num_nodes);
+
+    plotly_plotter::data_table data;
+    auto x_list = data.emplace<double>("x");
+    auto y_list = data.emplace<double>("y");
+    for (const auto& node : nodes) {
+        x_list->push_back(node.x());
+        y_list->push_back(node.y());
+    }
+
+    const auto figure =
+        plotly_plotter::figure_builders::scatter(data)
+            .x("x")
+            .y("y")
+            .title(fmt::format("{} Halton nodes in 2D", num_nodes))
+            .create();
+
+    plotly_plotter::write_html("2d_halton_nodes.html", figure);
+    plotly_plotter::write_png("2d_halton_nodes.png", figure);
 }
