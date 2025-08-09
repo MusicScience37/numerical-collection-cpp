527cdc74806b52bc11d12f2befebd3447f3b0025
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-08-09 06:28:15 +0000

    :white_check_mark: Add tests of concepts in base module.

---
+++ test/units/base/concepts/dense_matrix_of_test.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Test of dense_matrix_of concept.
+ */
+#include "num_collect/base/concepts/dense_matrix_of.h"
+
+#include <Eigen/Core>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("num_collect::concepts::dense_matrix_of") {
+    using num_collect::concepts::dense_matrix_of;
+
+    SECTION("check some types") {
+        STATIC_REQUIRE(dense_matrix_of<Eigen::MatrixXd, double>);
+        STATIC_REQUIRE(dense_matrix_of<Eigen::MatrixXf, float>);
+        STATIC_REQUIRE_FALSE(dense_matrix_of<Eigen::VectorXd, float>);
+    }
+}
---
+++ test/units/base/concepts/signed_integral_test.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Test of signed_integral concept.
+ */
+#include "num_collect/base/concepts/signed_integral.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("num_collect::base::concepts::signed_integral") {
+    using num_collect::base::concepts::signed_integral;
+
+    SECTION("check some types") {
+        STATIC_REQUIRE(signed_integral<int>);
+        STATIC_REQUIRE(signed_integral<signed int>);
+        STATIC_REQUIRE(signed_integral<long>);
+        STATIC_REQUIRE(signed_integral<signed long>);
+        STATIC_REQUIRE_FALSE(signed_integral<unsigned int>);
+        STATIC_REQUIRE_FALSE(signed_integral<float>);
+        STATIC_REQUIRE_FALSE(signed_integral<double>);
+    }
+}
