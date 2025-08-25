cd9823ac22afd162296edf76ee429abf96e3a9b0
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-08-25 13:56:15 +0000

    :sparkles: Class of RBF for polyharmonic splines.

--- test/units/rbf/unity_source.cpp
+++ test/units/rbf/unity_source.cpp
@@ -14,5 +14,6 @@
 #include "rbfs/inverse_multi_quadric_rbf_test.cpp"  // NOLINT(bugprone-suspicious-include)
 #include "rbfs/inverse_quadratic_rbf_test.cpp"  // NOLINT(bugprone-suspicious-include)
 #include "rbfs/multi_quadric_rbf_test.cpp"  // NOLINT(bugprone-suspicious-include)
+#include "rbfs/polyharmonic_spline_rbf_test.cpp"  // NOLINT(bugprone-suspicious-include)
 #include "rbfs/sech_rbf_test.cpp"        // NOLINT(bugprone-suspicious-include)
 #include "rbfs/wendland_csrbf_test.cpp"  // NOLINT(bugprone-suspicious-include)
