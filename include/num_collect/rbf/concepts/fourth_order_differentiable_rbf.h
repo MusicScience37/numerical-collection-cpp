aa69f18fec10d8c8773362c97c13dcf6cf1cfed0
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-08-31 01:03:46 +0000

    :recycle: Concepts of third-order and fourth-order differentiable RBFs.

---
+++ include/num_collect/rbf/concepts/fourth_order_differentiable_rbf.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of fourth_order_differentiable_rbf concept.
+ */
+#pragma once
+
+#include "num_collect/rbf/concepts/differentiable_rbf.h"
+#include "num_collect/rbf/concepts/third_order_differentiable_rbf.h"
+#include "num_collect/rbf/rbfs/differentiated.h"
+
+namespace num_collect::rbf::concepts {
+
+/*!
+ * \brief Concept of fourth-order differentiable RBFs.
+ *
+ * \tparam T Type.
+ */
+template <typename T>
+concept fourth_order_differentiable_rbf = third_order_differentiable_rbf<T> &&
+    differentiable_rbf<rbfs::differentiated_t<
+        rbfs::differentiated_t<rbfs::differentiated_t<T>>>>;
+
+}  // namespace num_collect::rbf::concepts
--- include/num_collect/rbf/operators/laplacian_gradient_operator.h
+++ include/num_collect/rbf/operators/laplacian_gradient_operator.h
@@ -26,7 +26,7 @@
 #include "num_collect/base/index_type.h"
 #include "num_collect/base/precondition.h"
 #include "num_collect/rbf/concepts/csrbf.h"
-#include "num_collect/rbf/concepts/second_order_differentiable_rbf.h"
+#include "num_collect/rbf/concepts/third_order_differentiable_rbf.h"
 #include "num_collect/rbf/distance_functions/euclidean_distance_function.h"
 #include "num_collect/rbf/operators/general_operator_evaluator.h"
 #include "num_collect/rbf/operators/operator_evaluator.h"
@@ -74,7 +74,7 @@ private:
  * variables.
  */
 template <base::concepts::real_scalar Variable,
-    concepts::second_order_differentiable_rbf RBF>
+    concepts::third_order_differentiable_rbf RBF>
 struct operator_evaluator<laplacian_gradient_operator<Variable>, RBF,
     distance_functions::euclidean_distance_function<Variable>>
     : general_operator_evaluator<
