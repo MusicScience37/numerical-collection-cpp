c14f0f9724ed33e1b397cf1e3ccd80182a373b83
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-09-02 13:42:49 +0000

    :white_check_mark: Check precision of derivatives with polynomial terms.

--- test/integ/rbf_interp_comb/operators/second_derivative.cpp
+++ test/integ/rbf_interp_comb/operators/second_derivative.cpp
@@ -31,6 +31,7 @@
 #include "num_collect/rbf/generate_halton_nodes.h"
 #include "num_collect/rbf/operators/laplacian_operator.h"
 #include "num_collect/rbf/rbf_interpolator.h"
+#include "num_collect/rbf/rbf_polynomial_interpolator.h"
 #include "num_collect/rbf/rbfs/gaussian_rbf.h"
 #include "num_collect/rbf/rbfs/wendland_csrbf.h"
 
@@ -46,6 +47,59 @@ TEMPLATE_TEST_CASE(
     using rbf_type = TestType;
     using rbf_interpolator_type =
         local_rbf_interpolator<variable_type(variable_type), rbf_type>;
+
+    rbf_interpolator_type interpolator;
+
+    const auto function = [](double x) {
+        return std::cos(num_collect::constants::pi<double> * x);
+    };
+    const auto second_derivative_function = [](double x) {
+        return -num_collect::constants::pi<double> *
+            num_collect::constants::pi<double> *
+            std::cos(num_collect::constants::pi<double> * x);
+    };
+
+    const auto sample_variables = generate_1d_halton_nodes<double>(10);
+    Eigen::VectorXd sample_values{};
+    sample_values.resize(
+        static_cast<num_collect::index_type>(sample_variables.size()));
+    for (std::size_t i = 0; i < sample_variables.size(); ++i) {
+        sample_values(static_cast<num_collect::index_type>(i)) =
+            function(sample_variables[i]);
+    }
+
+    interpolator.compute(sample_variables, sample_values);
+
+    const Eigen::VectorXd interpolated_variables =
+        Eigen::VectorXd::LinSpaced(11, 0.0, 1.0);
+    Eigen::VectorXd interpolated_values;
+    interpolated_values.resize(interpolated_variables.size());
+    Eigen::VectorXd actual_values;
+    actual_values.resize(interpolated_variables.size());
+    for (num_collect::index_type i = 0; i < interpolated_variables.size();
+        ++i) {
+        interpolated_values(i) = interpolator.evaluate(
+            laplacian_operator(interpolated_variables(i)));
+        actual_values(i) =
+            second_derivative_function(interpolated_variables(i));
+    }
+    comparison_approvals::verify_with_reference(
+        interpolated_values, actual_values, 2);
+}
+
+TEMPLATE_TEST_CASE_SIG(
+    "second-order derivative of global_rbf_polynomial_interpolator with "
+    "different degrees",
+    "", ((int Degree), Degree), (0), (1), (2)) {
+    using num_collect::rbf::generate_1d_halton_nodes;
+    using num_collect::rbf::local_rbf_interpolator;
+    using num_collect::rbf::operators::laplacian_operator;
+
+    using variable_type = double;
+    using rbf_type = num_collect::rbf::rbfs::gaussian_rbf<double>;
+    using rbf_interpolator_type =
+        num_collect::rbf::global_rbf_polynomial_interpolator<
+            double(variable_type), rbf_type, Degree>;
 
     rbf_interpolator_type interpolator;
 
