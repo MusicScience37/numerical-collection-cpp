afba95bedd7987b9bb0c3f0c8930bfe58201d467
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-09-13 13:49:57 +0000

    :books: Update JSON schema of configuration files.

--- config_schema/tests/constants.py
+++ config_schema/tests/constants.py
@@ -4,7 +4,7 @@ import os
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-SCHEMA_DIR = os.path.join(os.path.dirname(THIS_DIR), "v2")
+SCHEMA_DIR = os.path.join(os.path.dirname(THIS_DIR), "v3")
 
 ROOT_DIR = os.path.dirname(os.path.dirname(THIS_DIR))
 
