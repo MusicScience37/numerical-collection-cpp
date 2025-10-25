f42e11b7086f4a5bae28ac7e42b5a96d6073783c
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2025-10-25 12:39:51 +0000

    :bug: Fix compilation with MSVC.

--- test/units/base/constants_test.cpp
+++ test/units/base/constants_test.cpp
@@ -19,6 +19,8 @@
  */
 #include "num_collect/base/constants.h"
 
+#include <cmath>
+
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
