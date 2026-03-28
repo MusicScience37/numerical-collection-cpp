0dde48248ada94133195220ef2d33272fdb9e0c9
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-03-28 16:10:42 +0000

    :white_check_mark: Test of different degrees of polyharmonic splines in RBF-FD method.

--- test/integ/rbf_fd_comb/source_list.cmake
+++ test/integ/rbf_fd_comb/source_list.cmake
@@ -2,4 +2,5 @@ set(SOURCE_FILES
     dimensions/phs_rbf_fd_polynomial_assembler_poisson.cpp
     dimensions/rbf_fd_assembler_poisson.cpp
     polynomials/phs_rbf_fd_polynomial_assembler_poisson.cpp
+    rbfs/phs_degrees.cpp
 )
--- test/integ/rbf_fd_comb/unity_source.cpp
+++ test/integ/rbf_fd_comb/unity_source.cpp
@@ -1,3 +1,4 @@
 #include "dimensions/phs_rbf_fd_polynomial_assembler_poisson.cpp"
 #include "dimensions/rbf_fd_assembler_poisson.cpp"
 #include "polynomials/phs_rbf_fd_polynomial_assembler_poisson.cpp"
+#include "rbfs/phs_degrees.cpp"
