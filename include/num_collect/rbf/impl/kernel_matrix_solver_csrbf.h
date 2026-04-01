3ce31fc9d6aa436661c21055469c780cfd6266aa
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-04-01 13:49:18 +0000

    :recycle: Separate kernel_matrix_solver implementation into multiple files.

---
+++ include/num_collect/rbf/impl/kernel_matrix_solver_csrbf.h
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2026 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Definition of kernel_matrix_solver class for CSRBFs.
+ */
+#pragma once
+
+#include <cmath>
+
+#include <Eigen/Core>
+#include <Eigen/IterativeLinearSolvers>
+#include <Eigen/SparseCore>
+
+#include "num_collect/base/precondition.h"
+#include "num_collect/rbf/impl/kernel_matrix_solver_decl.h"
+#include "num_collect/rbf/kernel_matrix_type.h"
+
+namespace num_collect::rbf::impl {
+
+/*!
+ * \brief Class to solve linear equations of kernel matrices.
+ *
+ * \tparam KernelValue Type of values of the kernel.
+ * \tparam FunctionValue Type of the function values.
+ *
+ * This class solves the following linear equations:
+ *
+ * \f[
+ * K \boldsymbol{c} = \boldsymbol{y}
+ * \f]
+ *
+ * where
+ * - \f$ K \f$ is a kernel matrix,
+ * - \f$ \boldsymbol{c} \f$ is a vector of coefficients for the kernel,
+ * - \f$ \boldsymbol{y} \f$ is a vector of function values.
+ *
+ * This specialization handles the case where the kernel matrix is sparse and
+ * uses the localized length parameters which causes a non-symmetric kernel
+ * matrix.
+ */
+template <typename KernelValue, typename FunctionValue>
+class kernel_matrix_solver<KernelValue, FunctionValue,
+    kernel_matrix_type::sparse, false> {
+public:
+    //! Type of matrices.
+    using kernel_matrix_type =
+        Eigen::SparseMatrix<KernelValue, Eigen::RowMajor>;
+
+    //! Type of vectors.
+    using vector_type = Eigen::VectorX<FunctionValue>;
+
+    //! Type of scalars.
+    using scalar_type = KernelValue;
+
+    //! Constructor.
+    kernel_matrix_solver() = default;
+
+    /*!
+     * \brief Compute internal matrices.
+     *
+     * \param[in] kernel_matrix Kernel matrix.
+     * \param[in] data Data vector.
+     */
+    void compute(
+        const kernel_matrix_type& kernel_matrix, const vector_type& data) {
+        (void)data;  // not used in this version.
+
+        solver_.compute(kernel_matrix);
+    }
+
+    /*!
+     * \brief Solve the linear equation with a regularization parameter.
+     *
+     * \param[out] coefficients Vector of coefficients for each sample points.
+     * \param[in] reg_param Regularization parameter.
+     * \param[in] data Data vector.
+     */
+    void solve(vector_type& coefficients, scalar_type reg_param,
+        const vector_type& data) const {
+        NUM_COLLECT_PRECONDITION(reg_param == static_cast<scalar_type>(0),
+            "Non-zero regularization parameter cannot be used in this "
+            "configuration.");
+
+        coefficients = solver_.solve(data);
+    }
+
+private:
+    //! Solver of the kernel matrix.
+    Eigen::BiCGSTAB<kernel_matrix_type> solver_;
+};
+
+}  // namespace num_collect::rbf::impl
---
+++ include/num_collect/rbf/impl/kernel_matrix_solver_decl.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2026 MusicScience37 (Kenta Kabashima)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*!
+ * \file
+ * \brief Declaration of kernel_matrix_solver class.
+ */
+#pragma once
+
+#include <cmath>
+
+#include "num_collect/rbf/kernel_matrix_type.h"
+
+namespace num_collect::rbf::impl {
+
+/*!
+ * \brief Class to solve linear equations of kernel matrices.
+ *
+ * \tparam KernelValue Type of values of the kernel.
+ * \tparam FunctionValue Type of the function values.
+ * \tparam KernelMatrixType Type of the kernel matrices.
+ * \tparam UsesGlobalLengthParameter Whether to uses the globally fixed length
+ * parameters.
+ *
+ * This class solves the following linear equations:
+ *
+ * \f[
+ * K \boldsymbol{c} = \boldsymbol{y}
+ * \f]
+ *
+ * where
+ * - \f$ K \f$ is a kernel matrix,
+ * - \f$ \boldsymbol{c} \f$ is a vector of coefficients for the kernel,
+ * - \f$ \boldsymbol{y} \f$ is a vector of function values.
+ */
+template <typename KernelValue, typename FunctionValue,
+    kernel_matrix_type KernelMatrixType, bool UsesGlobalLengthParameter>
+class kernel_matrix_solver;
+
+}  // namespace num_collect::rbf::impl
