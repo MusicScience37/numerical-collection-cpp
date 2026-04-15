f0dcd0188dcfc4eb896aa3261a663007fe0b17bd
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-04-15 15:09:30 +0000

    :bug: Remove formulas which are not working at all.

--- test/integ/ode_comb/exponential/non_embedded.cpp
+++ test/integ/ode_comb/exponential/non_embedded.cpp
@@ -29,7 +29,6 @@
 #include "num_collect/ode/runge_kutta/lobatto3c6_formula.h"
 #include "num_collect/ode/runge_kutta/radau2a3_formula.h"
 #include "num_collect/ode/runge_kutta/radau2a5_formula.h"
-#include "num_collect/ode/runge_kutta/radau2a_formula.h"
 #include "num_collect/ode/runge_kutta/rk4_formula.h"
 #include "num_prob_collect/ode/exponential_problem.h"
 
@@ -39,8 +38,7 @@ TEMPLATE_PRODUCT_TEST_CASE("exponential_problem with non-embedded formulas", "",
         num_collect::ode::runge_kutta::lobatto3c4_solver,
         num_collect::ode::runge_kutta::lobatto3c6_solver,
         num_collect::ode::runge_kutta::radau2a3_solver,
-        num_collect::ode::runge_kutta::radau2a5_solver,
-        num_collect::ode::runge_kutta::radau2a9_solver),
+        num_collect::ode::runge_kutta::radau2a5_solver),
     (num_prob_collect::ode::exponential_problem)) {
     using problem_type = num_prob_collect::ode::exponential_problem;
     using solver_type = TestType;
