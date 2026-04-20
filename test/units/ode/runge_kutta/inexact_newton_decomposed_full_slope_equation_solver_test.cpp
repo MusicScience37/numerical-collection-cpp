c85910cef29e0732732b9a514045839e40ce41d4
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-04-20 15:42:25 +0000

    :white_check_mark: Tests of masses in scalar problems.

--- test/units/ode/runge_kutta/inexact_newton_decomposed_full_slope_equation_solver_test.cpp
+++ test/units/ode/runge_kutta/inexact_newton_decomposed_full_slope_equation_solver_test.cpp
@@ -31,6 +31,7 @@
 #include "num_collect/ode/problems/linear_first_order_dae_problem.h"
 #include "num_collect/ode/problems/linear_first_order_ode_problem.h"
 #include "num_collect/ode/runge_kutta/radau2a5_formula.h"
+#include "num_prob_collect/ode/changing_mass_exponential_problem.h"
 #include "num_prob_collect/ode/exponential_problem.h"
 #include "num_prob_collect/ode/implicit_exponential_problem.h"
 #include "num_prob_collect/ode/implicit_kaps_problem.h"
@@ -47,6 +48,72 @@ TEST_CASE(
 
     SECTION("solve a scalar problem without mass") {
         using problem_type = num_prob_collect::ode::exponential_problem;
+        using formula_type = radau2a5_formula<problem_type>;
+        using solver_type =
+            inexact_newton_decomposed_full_slope_equation_solver<problem_type,
+                formula_type::stages>;
+        using solver_data_type =
+            inexact_newton_decomposed_full_equation_solver_data<
+                solver_type::scalar_type, formula_type::stages>;
+        using slope_type = solver_type::slope_vector_type;
+
+        const solver_data_type data = solver_data_type::from_butcher_tableau(
+            formula_type::slope_coeffs(), formula_type::time_coeffs());
+        solver_type solver(data);
+
+        problem_type problem;
+        constexpr double time = 0.0;
+        constexpr double step_size = 1e-2;
+        constexpr double variable = 1.0;
+        slope_type solution = slope_type::Zero();
+        solver.init(problem, time, step_size, variable, solution);
+        REQUIRE_NOTHROW(solver.solve());
+
+        constexpr int stages = formula_type::stages;
+        const Eigen::Vector<double, stages> update_coeffs =
+            formula_type::update_coeffs();
+        const double actual =
+            variable + step_size * solution.dot(update_coeffs);
+        const double reference = std::exp(step_size);
+        comparison_approvals::verify_with_reference(actual, reference);
+    }
+
+    SECTION("solve a scalar problem with mass") {
+        using problem_type =
+            num_prob_collect::ode::implicit_exponential_problem;
+        using formula_type = radau2a5_formula<problem_type>;
+        using solver_type =
+            inexact_newton_decomposed_full_slope_equation_solver<problem_type,
+                formula_type::stages>;
+        using solver_data_type =
+            inexact_newton_decomposed_full_equation_solver_data<
+                solver_type::scalar_type, formula_type::stages>;
+        using slope_type = solver_type::slope_vector_type;
+
+        const solver_data_type data = solver_data_type::from_butcher_tableau(
+            formula_type::slope_coeffs(), formula_type::time_coeffs());
+        solver_type solver(data);
+
+        problem_type problem;
+        constexpr double time = 0.0;
+        constexpr double step_size = 1e-2;
+        constexpr double variable = 1.0;
+        slope_type solution = slope_type::Zero();
+        solver.init(problem, time, step_size, variable, solution);
+        REQUIRE_NOTHROW(solver.solve());
+
+        constexpr int stages = formula_type::stages;
+        const Eigen::Vector<double, stages> update_coeffs =
+            formula_type::update_coeffs();
+        const double actual =
+            variable + step_size * solution.dot(update_coeffs);
+        const double reference = std::exp(step_size);
+        comparison_approvals::verify_with_reference(actual, reference);
+    }
+
+    SECTION("solve a scalar problem with changing mass") {
+        using problem_type =
+            num_prob_collect::ode::changing_mass_exponential_problem;
         using formula_type = radau2a5_formula<problem_type>;
         using solver_type =
             inexact_newton_decomposed_full_slope_equation_solver<problem_type,
