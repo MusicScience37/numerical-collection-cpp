40215062d566dafa588412f8e206ec5a3eb75adf
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-04-20 15:19:01 +0000

    :white_check_mark: Separate tests of separated headers.

--- test/units/ode/runge_kutta/inexact_newton_decomposed_full_update_equation_solver_test.cpp
+++ test/units/ode/runge_kutta/inexact_newton_decomposed_full_update_equation_solver_test.cpp
@@ -26,7 +26,6 @@
 #include <catch2/matchers/catch_matchers.hpp>
 
 #include "comparison_approvals.h"
-#include "eigen_approx.h"
 #include "num_collect/base/constants.h"
 #include "num_collect/ode/problems/linear_first_order_dae_problem.h"
 #include "num_collect/ode/problems/linear_first_order_ode_problem.h"
@@ -35,42 +34,6 @@
 #include "num_prob_collect/ode/implicit_exponential_problem.h"
 #include "num_prob_collect/ode/implicit_kaps_problem.h"
 #include "num_prob_collect/ode/spring_movement_problem.h"
-
-TEST_CASE(
-    "num_collect::ode::runge_kutta::inexact_newton_decomposed_full_update_"
-    "equation_solver_data") {
-    using num_collect::ode::runge_kutta::
-        inexact_newton_decomposed_full_equation_solver_data;
-    using num_collect::ode::runge_kutta::radau2a5_formula;
-
-    SECTION("create data") {
-        using formula_type =
-            radau2a5_formula<num_prob_collect::ode::exponential_problem>;
-        const auto& slope_coeffs = formula_type::slope_coeffs();
-        const auto& time_coeffs = formula_type::time_coeffs();
-
-        using data_type =
-            inexact_newton_decomposed_full_equation_solver_data<double,
-                formula_type::stages>;
-        data_type data =
-            data_type::from_butcher_tableau(slope_coeffs, time_coeffs);
-
-        // Inputs.
-        CHECK_THAT(data.time_coeffs(), eigen_approx(time_coeffs));
-
-        // Eigenvectors.
-        const Eigen::Matrix3d eigenvectors_inverse_times_eigenvectors =
-            data.eigenvectors_inverse() * data.eigenvectors();
-        const Eigen::Matrix3d identity = Eigen::Matrix3d::Identity();
-        CHECK_THAT(
-            eigenvectors_inverse_times_eigenvectors, eigen_approx(identity));
-
-        // Block-diagonal matrix.
-        const Eigen::Matrix3d reconstructed_slope_coeffs = data.eigenvectors() *
-            data.block_diagonal_matrix() * data.eigenvectors_inverse();
-        CHECK_THAT(reconstructed_slope_coeffs, eigen_approx(slope_coeffs));
-    }
-}
 
 TEST_CASE(
     "num_collect::ode::runge_kutta::inexact_newton_decomposed_full_update_"
