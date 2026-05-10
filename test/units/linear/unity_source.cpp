82ea6586fbddcf37cc80e153803fb9b3c5d3ad53
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-05-10 13:32:14 +0000

    :white_check_mark: Tests of functional_preconditioned_gmres class.

--- test/units/linear/unity_source.cpp
+++ test/units/linear/unity_source.cpp
@@ -3,6 +3,7 @@
 #include "cuthill_mckee_ordering_test.cpp"
 #include "functional_bicgstab_test.cpp"
 #include "functional_gmres_test.cpp"
+#include "functional_preconditioned_gmres_test.cpp"
 #include "gauss_seidel_iterative_solver_test.cpp"
 #include "impl/amg/build_first_coarse_grid_candidate_test.cpp"
 #include "impl/amg/compute_strong_connection_list_test.cpp"
